16ce0faa1201983f7409c6420148e63a267fe6b2
Salma Farag Galal <salmafarag94@gmail.com>
2013-03-02 11:25:22 +0200

    Salma added the task

---
+++ framework_task/C3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task::Application.config.secret_token = '10c57e27b5f29ea6100fe53f9c30c325ea69d37a7077653c1b077ac1c0a1220196af9a505b4070fc716196026adc509865050c02adf1e6cfd455b43656a6e0f1'
