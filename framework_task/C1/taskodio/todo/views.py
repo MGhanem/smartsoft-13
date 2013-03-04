cdacf6939fcbdd56527a21871344477a65af9d0b
Adam G <ghanem.adam@gmail.com>
2013-03-04 23:55:59 +0200

    removed todo from git

--- framework_task/C1/taskodio/todo/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.template import Context, loader
-
-def home(request):
-	template = loader.get_template('todo/home.html')
-	context = ({'welcome_message': "Hello, from DJango!!"})
-	return HttpResponse(template.render(context))
