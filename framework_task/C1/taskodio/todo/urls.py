85b7eff361b8a1e62853909852c8575ad1c72281
Adam G <ghanem.adam@gmail.com>
2013-03-04 23:29:18 +0200

    pushing for z to see

---
+++ framework_task/C1/taskodio/todo/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+from todo import views
+# urlpatterns = patterns('',
+# 	# /polls/
+#     url(r'^$', views.index, name='index'),
+#     # /polls/5
+#     url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+#     # /polls/5/results
+#     url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+#     # /polls/5/vote
+#     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+# )
+urlpatterns = patterns('',
+	url(r'^$', views.home, name='home'),
+)
--- framework_task/C1/taskodio/todo/views.py
+++ framework_task/C1/taskodio/todo/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def home(request):
+	template = loader.get_template('todo/home.html')
+	context = ({'welcome_message': "Hello, from DJango!!"})
+	return HttpResponse(template.render(context))
