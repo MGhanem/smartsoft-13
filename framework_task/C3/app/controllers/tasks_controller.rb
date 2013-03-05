4b7da36bf4e7357c757f769580389b5d5b240ace
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-03-05 15:07:10 +0200

    Mohamed added files

--- framework_task/C3/app/controllers/tasks_controller.rb
+++ framework_task/C3/app/controllers/tasks_controller.rb
@@ -1,13 +1,32 @@
 class TasksController < ApplicationController
-	def create
-    @list = List.find(params[:list_id])
-    @task = @list.tasks.create(params[:task])
-    redirect_to list_path(@list)
+	
+    def create
+        @list = List.find(params[:list_id])
+        @task = @list.tasks.create(params[:task])
+        redirect_to list_path(@list)
 	end
-	def destroy
-    @list = List.find(params[:list_id])
-    @task = @list.tasks.find(params[:id])
-    @task.destroy
-    redirect_to list_path(@list)
-  end
+	
+    def destroy
+        @list = List.find(params[:list_id])
+        @task = @list.tasks.find(params[:id])
+        @task.destroy
+        redirect_to list_path(@list)
+    end
+    
+    def edit
+        @list = List.find(params[:list_id])
+        @task = @list.tasks.find(params[:id])
+    end
+
+    def update
+        @list = List.find(params[:list_id])
+        @task = @list.tasks.find(params[:id])
+
+        if @task.update_attributes(params[:task])
+            redirect_to(@list)
+        end
+    end
+
+    
+
 end
