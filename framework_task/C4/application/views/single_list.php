7cb155f776d7dc5533e5ed8485bd62bc892db30a
Mostafa Ahmed Hassaan <mostafa.a.hassaan@gmail.com>
2013-03-05 16:07:31 +0200

    Mostafa Updated files

--- framework_task/C4/application/views/single_list.php
+++ framework_task/C4/application/views/single_list.php
@@ -3,21 +3,70 @@
 	<title>List</title>
 </head>
 <body>
+	<form  action="/index.php/tasklist/viewall/">­
+    <input type="Submit" value="Back">
+    </form>
+
 
 	<?php
 		echo 'ID: ' . $list->id . '<br />';
         echo 'Name: ' . $list->name . '<br />';
         echo 'Owner ID: ' . $list->owner_id . '<br /><br />';
 	?>
+
+	<br>
+	<h1>Tasks:</h1>
+	<br>
+
 	
-	<form action="/index.php/tasklist/delete/<?php echo $list->id;?>">­
+
+	<?php
+
+		
+
+		foreach ($task as $Task_model)
+        {
+            echo 'ID: ' . $Task_model->id . '<br />';
+            echo 'Text: ' . $Task_model->text . '<br />';?>
+
+
+            <form style='display:inline;' action="/index.php/task/edit/<?php echo $Task_model->id;?>">­
+		    <input type="Submit" value="Edit Task">
+		    </form>
+
+            <form style='display:inline;' action="/index.php/task/delete/<?php echo $Task_model->id;?>">­
+		    <input type="Submit" value="Delete Task">
+		    </form>
+
+		    <form style='display:inline;' action="#">­
+		    <input type="Submit" value="Mark task as Done ">
+		    </form>
+
+		    <br><br><br>
+		    <?php
+           
+
+            
+        }
+	
+     ?>
+
+
+
+
+     <br>
+
+
+	<form style='display:inline;' action="/index.php/task/create/<?php echo $list->id;?>">­
+    <input type="Submit" value="Create New Task">
+    </form>
+	
+	<form  action="/index.php/tasklist/delete/<?php echo $list->id;?>">­
     <input type="Submit" value="Delete List">
     </form>
-    
-    <form action="/index.php/tasklist/viewall/">­
-    <input type="Submit" value="Back">
-    </form>
 
+    <br>
+    
 	
 </body>
 </html>
