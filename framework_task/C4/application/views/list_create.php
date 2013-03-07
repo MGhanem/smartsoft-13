d1d07e57c868e69463e9584ceb35e03f5d2b7ffc
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-03-07 14:34:18 +0200

    added unadded files

---
+++ framework_task/C4/application/views/list_create.php
@@ -0,0 +1,7 @@
+  <form action="/index.php/task/create/>" method="post">
+   <span>List: </span>
+   <input type="text" name="text" value="" />
+   <input type="submit" name="submit" value="Add Task" />
+  </form>
+</body>
+</html>
