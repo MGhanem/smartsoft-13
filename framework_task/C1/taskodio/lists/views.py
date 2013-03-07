44831e0907abc7c187737edadf82f1db8d3f1f9b
Adam G <ghanem.adam@gmail.com>
2013-03-07 08:53:20 +0200

    fixed nonsigned in root redirect if not signed in

--- framework_task/C1/taskodio/lists/views.py
+++ framework_task/C1/taskodio/lists/views.py
@@ -6,13 +6,13 @@ from lists.models import List
 from django.contrib.auth.models import User
 
 
-def list_manage(request):
-	template = loader.get_template('lists/list_manage.html')
-	context = Context({
-			'error': 'The email you have entered is already in use.',
-			'user': request.user
-		})
-	return HttpResponse(template.render(context))
+# def list_manage(request):
+# 	template = loader.get_template('lists/list_manage.html')
+# 	context = Context({
+# 			'error': 'The email you have entered is already in use.',
+# 			'user': request.user
+# 		})
+# 	return HttpResponse(template.render(context))
 
 # s5 create list view redirect
 def new_list(request):
@@ -63,6 +63,11 @@ def create_list(request):
 
 #s4
 def view_lists(request):
+	if not request.user.is_authenticated():
+		# redirect to the sign in page and send error with it
+		context = Context({ 'errors': "You need to sign in before viewing your lists."})
+		return render_to_response('accounts/signin.html', context, RequestContext(request))
+
 	if request.user.is_authenticated():
 		user = request.user
 		list_name_set = user.list_set.all()
@@ -70,10 +75,10 @@ def view_lists(request):
 		#return HttpResponse(list_name_set.all())
 		# return render(request, 'lists/list_manage.html', context)
 		return render_to_response('lists/list_manage.html', context, RequestContext(request))
-	else:
-		list_name_set = user.list_set.all()
-		context = Context({
-			'list_name_set': list_name_set,
-			'user': user,
-		})
-		return render(request,'lists/list_manage.html',context)
+	# else:
+	# 	list_name_set = user.list_set.all()
+	# 	context = Context({
+	# 		'list_name_set': list_name_set,
+	# 		'user': user,
+	# 	})
+	# 	return render(request,'lists/list_manage.html',context)
