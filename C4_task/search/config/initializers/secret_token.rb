9a902eeb4cc0a6b96a4ee3af36c3d056cda72f04
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-03-13 12:38:37 +0200

    init for simulated task

---
+++ C4_task/search/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = 'd0d047661858b4cea583ddd6ac9fc752d4f88c980f9668fbb75cff0ffd62cfd09b8f57169fb64ebc7e65bf0d9ca75effef26870fb0a8e260321968a710c52fec'
