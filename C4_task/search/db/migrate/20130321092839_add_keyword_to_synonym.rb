044fcb176eeeaa0bcf2d0f8047c0a306da6d6d62
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-03-21 11:53:38 +0200

    resolved migration errors

--- C4_task/search/db/migrate/20130321092839_add_keyword_to_synonym.rb
+++ C4_task/search/db/migrate/20130321092839_add_keyword_to_synonym.rb
@@ -1,6 +1,7 @@
 class AddKeywordToSynonym < ActiveRecord::Migration
   def change
-  	change_table :synonym do |syn|
-  		syn.references :keyword
+  	change_table :synonyms do |syn|
+  		syn.references :keywords
+  	end
   end
 end
