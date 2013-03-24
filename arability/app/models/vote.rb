a7f95369c6137196ab195540890203c8dc2cf075
nourhanZ <nourhan.nz@gmail.com>
2013-03-24 16:52:20 -0700

    [Issue #96][WIP] added validations to vote uniqueness

--- arability/app/models/vote.rb
+++ arability/app/models/vote.rb
@@ -2,17 +2,22 @@ class Vote < ActiveRecord::Base
   belongs_to :synonym
   belongs_to :gamer
   # attr_accessible :title, :body
+  validates_uniquness_of :gamer_id, :synonym_id 
 
    # This method is used to record the vote given for a certain synonym by a ceratin user
-   # The input is the gamer_id which is the voter_id and the synonym_id that the gamer voted for
-   # The method returns the created instance of model vote if the vote was correctly saved and
-   # return nil otherwise.
+   # Parameters:
+   #  gamer_id: the voter ID
+   #  synonym_id: the synonym_id that the gamer voted for
+   # Returns:
+   #  On success: Returns the instance of vote that was created and saved
+   #  On failure: returns nil
 
 
   def self.recordVote (gamer_id, synonym_id)
        valid_gamer = Gamer.find_by_id(gamer_id)
        valid_synonym = Synonym.find_by_id(synonym_id)
         if valid_gamer!=nil && valid_synonym!=nil 
+          if Vote.find_by_synonym_id()
           @vote = Vote.new
           @vote.synonym_id = synonym_id
           @vote.gamer_id = gamer_id
