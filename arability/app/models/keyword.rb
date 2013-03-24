87b711172a2556eb6ed7b27d5cb2494b36b54cd0
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-03-24 15:16:04 +0200

    Task implemented except for UML and Documentation #76

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,3 +1,8 @@
 class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
+  has_many :synonyms
+
+  def self.words_with_unapproved_synonyms
+  	return Keyword.joins(:synonyms).where("synonyms.approved" => false).all
+  end
 end
--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -1,4 +1,5 @@
 class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
+
 end
