1bc05315c52d796b0038eaec18efec895b976e45
Kareem <lkareem.alil@gmail.com>
2013-03-24 20:28:18 +0200

    Adding migrations of add devise gem to the Gamer and modifing the gamer migration

--- arability/db/migrate/20130323185108_create_gamers.rb
+++ arability/db/migrate/20130323185108_create_gamers.rb
@@ -1,6 +1,13 @@
 class CreateGamers < ActiveRecord::Migration
   def change
     create_table :gamers do |t|
+    	
+		t.string :name
+		t.string :email
+		t.string :username
+		t.integer :sign_up_status , :default => 0
+		t.string :country
+		t.date :date_of_birth
 
       t.timestamps
     end
