2aad69652508cf84bbce6c8d49713f1ba701f28f
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-03-24 16:17:28 +0200

    [Issue #76] Code implemented, documented.

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -2,7 +2,14 @@ class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
   has_many :synonyms
 
+  #Method takes no inputs and returns an array of "Keywords"
+  #with "synonyms" that haven't been approved yet.
+  #params: none
+  #returns:
+  # => on success: Array of "keywords"
+  # => on failure: Empty array
   def self.words_with_unapproved_synonyms
   	return Keyword.joins(:synonyms).where("synonyms.approved" => false).all
   end
+
 end
