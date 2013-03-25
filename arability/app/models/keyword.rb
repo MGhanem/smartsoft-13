78efb4ebae323674c885f04cf2dd7d4e430af6ef
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-03-25 16:49:56 +0200

    [issue #76] [WIP] commiting changes for pull

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -2,12 +2,12 @@ class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
   has_many :synonyms
 
-  #Method takes no inputs and returns an array of "Keywords"
-  #with "synonyms" that haven't been approved yet.
-  #params: none
-  #returns:
-  # => on success: Array of "keywords"
-  # => on failure: Empty array
+  # Method takes no inputs and returns an array of "Keywords"
+  # with "synonyms" that haven't been approved yet.
+  # params: none
+  # returns:
+  #   on success: Array of "keywords"
+  #   on failure: Empty array
   def self.words_with_unapproved_synonyms
   	return Keyword.joins(:synonyms).where("synonyms.approved" => false).all
   end
--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -1,5 +1,6 @@
 class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
+  has_many :votes
 
 end
