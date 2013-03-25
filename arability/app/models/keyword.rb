6c630001eb833dfd8e153a79de34f96eede59d71
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-03-25 13:43:01 +0200

    [Issue #74][WIP] Added documentation: Searching for keywords

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,7 +1,18 @@
 class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
- 
+
+	#Description:
+	#	params:
+	#		search_word: a string representing the search keyword that should be retrieved if found in the database
+	#	returns:
+	#		success: 
+	#			returns a list of the keywords similar to the search keyword sorted in lexicographical order
+	#		failure:
+	#			returns an empty list if the search keyword had no matches or no similar keywords were found
   def self.get_similar_keywords(search_word)
+  	if (search_word.blank?)
+  		return []
+  	end
   	keyword_list = self.find(:all, :conditions => ['name LIKE ?', "%#{search_word}%"])
   	relevant_first_list = keyword_list.sort_by {|keyword| keyword.name.index(search_word) && keyword.name}
   	return relevant_first_list
