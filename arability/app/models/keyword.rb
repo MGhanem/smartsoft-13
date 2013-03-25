c688e26d3d5f7e523b7c56703947382ee0ca9506
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-25 17:12:37 +0200

    implementation and documentation added

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,7 +1,15 @@
 class Keyword < ActiveRecord::Base
   	attr_accessible :approved, :is_english, :name
-  	def self.approveKeyword(id)
-		@keyword = Keyword.find(id)
-		@keyword.update_attribute(:approved,true)
+
+  	# This method is to record the aproval of the admin to a certain keyword
+  	# Parameters:
+  	#  id: the id of the keyword to be approved
+  	# returns:
+  	#  on success of recording the approval: true
+  	#  on failure of recording the approval: false
+  	def self.approve_keyword(id)
+		keyword = Keyword.find(id)
+		keyword.approved = true
+	    return keyword.save
 	end
 end
