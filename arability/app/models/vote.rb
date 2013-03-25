50f6092e6509b88c486d1433d55170807027a968
nourhanZ <nourhan.nz@gmail.com>
2013-03-25 16:38:00 -0700

    [Issue #96][WIP] validations is still missing

--- arability/app/models/vote.rb
+++ arability/app/models/vote.rb
@@ -2,7 +2,10 @@ class Vote < ActiveRecord::Base
   belongs_to :synonym
   belongs_to :gamer
   # attr_accessible :title, :body
-  validates_uniqueness_of :gamer_id, :synonym_id 
+  #validates_uniqueness_of :gamer_id, :scope => :synonym_id
+  #validates_existence_of :gamer_id
+  #validates_existence_of :synonym_id
+
 
    # This method is used to record the vote given for a certain synonym by a ceratin user
    # Parameters:
@@ -11,13 +14,10 @@ class Vote < ActiveRecord::Base
    # Returns:
    #  On success: Returns the instance of vote that was created and saved
    #  On failure: returns nil
-
-
   def self.recordVote (gamer_id, synonym_id)
        valid_gamer = Gamer.find_by_id(gamer_id)
        valid_synonym = Synonym.find_by_id(synonym_id)
         if valid_gamer!=nil && valid_synonym!=nil 
-          if Vote.find_by_synonym_id()
           @vote = Vote.new
           @vote.synonym_id = synonym_id
           @vote.gamer_id = gamer_id
