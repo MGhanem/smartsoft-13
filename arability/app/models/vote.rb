7adac77a5fe6de987899f41656bdd796af808fd1
nourhanZ <nourhan.nz@gmail.com>
2013-03-26 16:30:50 -0700

    [Issue #96][Issue #93]added the final tested version of method that records votes and method that returns list of unvoted keywords

--- arability/app/models/vote.rb
+++ arability/app/models/vote.rb
@@ -2,10 +2,8 @@ class Vote < ActiveRecord::Base
   belongs_to :synonym
   belongs_to :gamer
   # attr_accessible :title, :body
-  #validates_uniqueness_of :gamer_id, :scope => :synonym_id
-  #validates_existence_of :gamer_id
-  #validates_existence_of :synonym_id
-
+  validate :validate_gamer_exists, :validate_synonym_exists
+  validates :gamer_id, :uniqueness => { :scope => :synonym_id}
 
    # This method is used to record the vote given for a certain synonym by a ceratin user
    # Parameters:
@@ -14,10 +12,7 @@ class Vote < ActiveRecord::Base
    # Returns:
    #  On success: Returns the instance of vote that was created and saved
    #  On failure: returns nil
-  def self.recordVote (gamer_id, synonym_id)
-       valid_gamer = Gamer.find_by_id(gamer_id)
-       valid_synonym = Synonym.find_by_id(synonym_id)
-        if valid_gamer!=nil && valid_synonym!=nil 
+  def self.record_vote(gamer_id, synonym_id) 
           @vote = Vote.new
           @vote.synonym_id = synonym_id
           @vote.gamer_id = gamer_id
@@ -26,8 +21,52 @@ class Vote < ActiveRecord::Base
             else
               return nil
    	        end
-   	    end
   end
 
+   # This method is used to retreive a list of specific size of keywords that gamer with this gamer_id didn't vote on yet
+   # Parameters:
+   #  gamer_id: the gamer ID 
+   #  count: the size of the list to be retreived
+   #  lang: integer to specify the language of keywords to be retreived if 0 then english only, if 1 then arabic only, otherwise both english and arabic 
+   #        keywords can be icluded
+   # Returns:
+   #  On success: Returns a list of un voted keywords of the specified langauge with size = count for the gamer with this gamer_id  
+   #  On failure: Returns empty list
+  def self.get_unvoted_keywords(gamer_id, count, lang)
+      if Gamer.find_by_id(gamer_id) != nil
+        voted_synonyms = Vote.where('gamer_id = ?', gamer_id).select('synonym_id')
+        voted_keywords = Synonym.where(:id => voted_synonyms).select('keyword_id')
+        #un_voted_keywords = Keyword.all - Keyword.where( :id => voted_keywords)
+          if lang == 0
+            un_voted_keywords = Keyword.where("is_english =?",true) - Keyword.where(:id => voted_keywords)
+          elsif lang ==1
+            un_voted_keywords = Keyword.where("is_english=?",false) - Keyword.where(:id => voted_keywords)
+          else
+            un_voted_keywords = Keyword.all - Keyword.where( :id => voted_keywords) 
+          end
+        list_returned = un_voted_keywords.take(count)
+
+        return list_returned
+
+      else
+        return nil
+      end
+  end
+
+  #This is a custom validation method that validates that there exists a gamer with this gamer_id 
+  def validate_gamer_exists
+    valid_gamer = Gamer.find_by_id(gamer_id)
+    if valid_gamer == nil
+      errors.add(:gamer_id,"this gamer_id soesn't exist")
+    end
+  end
+
+  #This is a custom validation method that validates that there exists a synonym with this synonym_id 
+  def validate_synonym_exists
+    valid_synonym = Synonym.find_by_id(synonym_id)
+    if valid_synonym == nil
+      errors.add(:synonym_id,"this synonym_id soesn't exist")
+    end
+  end
 
 end
