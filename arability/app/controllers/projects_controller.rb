73221b795974e7c2346998274869c20dcd19f4fd
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-03-26 18:21:36 +0200

    [Issue #75] [WIP] Modified index view and controller

--- arability/app/controllers/projects_controller.rb
+++ arability/app/controllers/projects_controller.rb
@@ -2,8 +2,12 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-  	 @projects = Project.all
-
+ 	@developer = Developer.where(:gamer_id => current_gamer.id).first
+  	if @developer.present?
+  		@projects = Project.where(:developer => @developer.id)
+  	else
+  		flash[:notice] = "You are not authorized to view this page"
+  	end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }
