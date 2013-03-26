5cf567b88529253f765890ebfb31737583acca6f
Salma Farag Galal <salmafarag94@gmail.com>
2013-03-26 23:35:33 +0200

    Salma commited

--- arability/app/controllers/projects_controller.rb
+++ arability/app/controllers/projects_controller.rb
@@ -1,44 +1,16 @@
 class ProjectsController < ApplicationController
-  # GET /projects
-  # GET /projects.json
+  #A method that views all projects
   def index
     @projects = Project.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @projects }
     end
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
-  def show
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
-  end
-
-  # GET /projects/new
-  # GET /projects/new.json
-  def new
-    @project = Project.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
-  end
-
-  # GET /projects/1/edit
-  def edit
-    @project = Project.find(params[:id])
-  end
-
-  # POST /projects
-  # POST /projects.json
+  #A method that calls method createproject that creates the project and redirects to the
+  #project page and prints an error if the data entered is invalid
   def create
     @project = Project.createproject(params[:project])
     respond_to do |format|
@@ -51,32 +23,39 @@ class ProjectsController < ApplicationController
       end
     end
   end
+#  def show
+ #   @project = Project.find(params[:id])
+   # respond_to do |format|
+    #  format.html
+     # format.json { render json: @project }
+    #end
+  #end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
-  def update
-    @project = Project.find(params[:id])
+ # def new
+  #  @project = Project.new
 
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   # respond_to do |format|
+    #  format.html
+     # format.json { render json: @project }
+    #end
+  #end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
-  def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
+  #def edit
+   # @project = Project.find(params[:id])
+  #end
 
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
-  end
+
+ # def update
+  #  @project = Project.find(params[:id])
+
+   # respond_to do |format|
+    #  if @project.update_attributes(params[:project])
+     #   format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+      #  format.json { head :no_content }
+      #else
+       # format.html { render action: "edit" }
+        #format.json { render json: @project.errors, status: :unprocessable_entity }
+      #end
+    #end
+  #end
 end
