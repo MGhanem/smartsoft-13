373c62e1d3f5beefc604f5cc0d032095f2133f32
Khloud Khalid <khloud.khalid7@gmail.com>
2013-03-27 17:23:03 +0200

    khloud added subscription model to my_subscriptions

---
+++ arability/db/migrate/20130327151044_add_subscription_model_to_my_subscription.rb
@@ -0,0 +1,7 @@
+class AddSubscriptionModelToMySubscription < ActiveRecord::Migration
+  def change
+  	change_table :my_subscriptions do |t|
+   		t.references :subscription_models
+   		
+  end
+end
