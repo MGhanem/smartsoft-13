4360e50c99355c56c99aad528f93e2a02f506c08
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-28 00:17:10 +0200

    rails code conventionsadded

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -2,23 +2,25 @@ class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
 
-  	# Author:
-    #   Mirna Yacout
-    # Description:
-    #   This method is to record the aproval of the admin to a certain synonym in the database
-    # Parameters:
-    #   id: the id of the synonym to be approved
-    # Success:
-    #   returns true on saving the approval correctly in the database
-    # Failure:
-    #   returns false if the synonym doesnot exist in the database
-    #   or if the approval failed to be saved in the database 
-  	def self.approve_synonym(id)
+	# Author:
+  #  Mirna Yacout
+  # Description:
+  #  This method is to record the aproval of the admin to a certain synonym in the database
+  # Parameters:
+  #  id: the id of the synonym to be approved
+  # Success:
+  #  returns true on saving the approval correctly in the database
+  # Failure:
+  #  returns false if the synonym doesnot exist in the database
+  #  or if the approval failed to be saved in the database 
+	class << self
+    def approve_synonym(id)
       if (Synonym.exists?(id))
         synonym = Synonym.find(id)
         synonym.approved = true
-        return synonym.save
+      return synonym.save
       end
       return false
-	end
+    end
+  end
 end
