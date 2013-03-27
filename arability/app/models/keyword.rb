f73522c496ffca77ba921a01b903a486a45b23db
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-03-27 14:37:06 +0200

    Omar finished implementation and documentation, and UML in progress [issue #89]

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,3 +1,18 @@
 class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
+
+# description:
+# 	feature takes no input and returns a list of all unapproved keywords
+# success: 
+# 	takes no arguments and returns to the admin a list containing the keywords 
+# 	that are pending for approval in the database
+# failure:
+# 	returns an empty list if no words are pending for approval
+
+  def self.unapprovedkeywords
+
+  	return Keyword.where("approved" => false).all
+
+  end
+
 end
