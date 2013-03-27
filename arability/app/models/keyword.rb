6d3b8f586d3754054ab2967b52ffb6540fcede6c
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-27 18:58:54 +0200

    UML added and documentation uodated

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,13 +1,15 @@
 class Keyword < ActiveRecord::Base
   	attr_accessible :approved, :is_english, :name
 
-  	# This method is to record the aproval of the admin to a certain keyword
-    # Author: Mirna Yacout
+    # Description:
+  	#  This method is to record the aproval of the admin to a certain keyword in the database
   	# Parameters:
   	#  id: the id of the keyword to be approved
-  	# returns:
-  	#  on success of recording the approval: true
-  	#  on failure of recording the approval: false
+  	# Success:
+  	#  returns true on saving the approval correctly in the database
+  	# Failure:
+    #  returns false if the keyword doesnot exist in the database
+    #  or if the approval failed to be saved in the database 
   	def self.approve_keyword(id)
 	    if (Keyword.exists?(id))
         keyword = Keyword.find(id)
