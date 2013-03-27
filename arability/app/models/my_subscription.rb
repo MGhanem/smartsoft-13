02aeb9f87a9e284aa09dd593be7a50277026ae30
Noha Hesham <nohahesham16@gmail.com>
2013-03-27 22:25:05 +0200

    noha committed

--- arability/app/models/my_subscription.rb
+++ arability/app/models/my_subscription.rb
@@ -5,19 +5,20 @@ class MySubscription < ActiveRecord::Base
 
   
   def self.decrement_limit(dev_id)
-
   	mySubscription = MySubscription.joins(:developer).where(:developer_id => dev_id).first
     old_limit = mySubscription.word_search
-  	if old_limit > 0
+  	if  mySubscription.word_search > 0
   	mySubscription.word_search = old_limit - 1
     mySubscription.save
     return true
     else
-
       return false
-
     end
   end
+#   def self.decrement!(word_search, by = 1)
+#     mySubscription = MySubscription.joins(:developer).where(:developer_id => dev_id).first
+#   decrement(word_search, by).update_attribute(word_search, self[word_search])
+# end
 
   def self.search_word_permission(dev_id)
     mySubscription = MySubscription.joins(:developer).where(:developer_id => dev_id).first
@@ -29,7 +30,7 @@ class MySubscription < ActiveRecord::Base
   end
 
   def self.add_word_permission(dev_id)
-     mySubscription = MySubscription.where(:developer => dev_id).first
+     mySubscription = MySubscription.joins(:developer).where(:developer_id => dev_id).first
   	if mysubscription.word_add > 0
   		return true
   	else 
@@ -38,6 +39,7 @@ class MySubscription < ActiveRecord::Base
   end
 
   def self.follow_word_permission(dev_id)
+    mySubscription = MySubscription.joins(:developer).where(:developer_id => dev_id).first
   	if mysubscription.word_follow > 0
   	    return true
   	else
