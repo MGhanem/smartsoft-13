f6c132ddbb8c2362249b5c885766096ab1138245
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-27 19:04:45 +0200

    documentation updated and UML added

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -2,13 +2,15 @@ class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
 
-  	# This method is to record the aproval of the admin to a certain synonym
-    # Author: Mirna Yacout
-  	# Parameters:
-  	#  id: the id of the synonym to be approved
-  	# returns:
-  	#  on success of recording the approval: true
-  	#  on failure of recording the approval: false
+  	# Description:
+    #  This method is to record the aproval of the admin to a certain synonym in the database
+    # Parameters:
+    #  id: the id of the synonym to be approved
+    # Success:
+    #  returns true on saving the approval correctly in the database
+    # Failure:
+    #  returns false if the synonym doesnot exist in the database
+    #  or if the approval failed to be saved in the database 
   	def self.approve_synonym(id)
       if (Synonym.exists?(id))
         synonym = Synonym.find(id)
