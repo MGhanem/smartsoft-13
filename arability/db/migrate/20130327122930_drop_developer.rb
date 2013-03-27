06350782c30fb90508faaebd757317fd0b3c0eb2
Khloud Khalid <khloud.khalid7@gmail.com>
2013-03-27 14:50:31 +0200

    khloud fixed developer migration

--- arability/db/migrate/20130327122930_drop_developer.rb
+++ arability/db/migrate/20130327122930_drop_developer.rb
@@ -1,13 +1,11 @@
 class DropDeveloper < ActiveRecord::Migration
    def up
-    change_table :developers do |t|
-    	t.references :gamer
-		t.string :first_name
-		t.string :last_name
-		t.boolean :verified 
-		t.boolean :blocked , :default => 'false'
-    end
-  end
+   	change_table :developers do |t|
+   		t.string :first_name
+   		t.string :last_name
+   		t.boolean :verified 
+   	end
+   end
 
   def down
   end
