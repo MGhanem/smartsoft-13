ea2790b353cd3230aa12d0420a4d58f181a45e64
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-28 00:11:25 +0200

    rails conventions added

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -1,23 +1,25 @@
 class Keyword < ActiveRecord::Base
-  	attr_accessible :approved, :is_english, :name
+  attr_accessible :approved, :is_english, :name
 
-    # Author:
-    #  Mirna Yacout
-    # Description:
-  	#  This method is to record the aproval of the admin to a certain keyword in the database
-  	# Parameters:
-  	#  id: the id of the keyword to be approved
-  	# Success:
-  	#  returns true on saving the approval correctly in the database
-  	# Failure:
-    #  returns false if the keyword doesnot exist in the database
-    #  or if the approval failed to be saved in the database 
-  	def self.approve_keyword(id)
-	    if (Keyword.exists?(id))
+  # Author:
+  #  Mirna Yacout
+  # Description:
+	#  This method is to record the aproval of the admin to a certain keyword in the database
+	# Parameters:
+	#  id: the id of the keyword to be approved
+	# Success:
+	#  returns true on saving the approval correctly in the database
+	# Failure:
+  #  returns false if the keyword doesnot exist in the database
+  #  or if the approval failed to be saved in the database 
+  class << self
+    def approve_keyword(id)
+      if (Keyword.exists?(id))
         keyword = Keyword.find(id)
         keyword.approved = true
         return keyword.save
       end
       return false
-	end
+    end
+  end
 end
