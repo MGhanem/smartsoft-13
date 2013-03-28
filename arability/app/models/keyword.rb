00b4039f3df5c7feda9595703f4bd79dceac39ff
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-28 02:43:21 +0200

    indentation

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -4,11 +4,11 @@ class Keyword < ActiveRecord::Base
   # Author:
   #   Mirna Yacout
   # Description:
-	#  This method is to record the aproval of the admin to a certain keyword in the database
+  #   This method is to record the aproval of the admin to a certain keyword in the database
 	# Parameters:
-	#  id: the id of the keyword to be approved
+  #   id: the id of the keyword to be approved
 	# Success:
-	#  returns true on saving the approval correctly in the database
+  #   returns true on saving the approval correctly in the database
 	# Failure:
   #   returns false if the keyword doesnot exist in the database
   #   or if the approval failed to be saved in the database 
