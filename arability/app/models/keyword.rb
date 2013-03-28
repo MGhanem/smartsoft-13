e094c149df0672b1607796bbbc5f3fa0381f296a
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-03-28 16:19:31 +0200

    Omar modified his documentation and replaced tabs with spaces[issue #89]

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -2,14 +2,14 @@ class Keyword < ActiveRecord::Base
   attr_accessible :approved, :is_english, :name
 
 # author:
-# 	Omar Hossam
+#   Omar Hossam
 # description:
-# 	feature takes no input and returns a list of all unapproved keywords
+#   feature takes no input and returns a list of all unapproved keywords
 # success: 
-# 	takes no arguments and returns to the admin a list containing the keywords 
-# 	that are pending for approval in the database
+#   takes no arguments and returns to the admin a list containing the keywords 
+#   that are pending for approval in the database
 # failure:
-# 	returns an empty list if no words are pending for approval
+#   returns an empty list if no words are pending for approval
 
   def self.listunapprovedkeywords
 
