33aa6c4f011dc071d8ad67c666dbe8083afae2dd
karim el naggar <karimfarid.naggar@gmail.com>
2013-03-28 16:57:17 +0200

    fixed docs style

--- arability/app/controllers/admin_controller.rb
+++ arability/app/controllers/admin_controller.rb
@@ -5,9 +5,16 @@ class AdminController < ApplicationController
 
   before_filter :check_login, :only => [:login]
   
-  #  returns:
-  #    success: if the user is not logged in redirect to login
-  #    failure: do nothing
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     a filter that makes sure the user is logged in
+  # params
+  #     none
+  # success: 
+  #     if the user is not logged in redirect to login
+  # failure: 
+  #     none
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in"
@@ -15,31 +22,74 @@ class AdminController < ApplicationController
     end
   end
 
-  #  returns:
-  #    success: redirect to index if the user is logged in
-  #    failure: does nothing
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     a function that checks if the user is logged in and redirects him to /admin/index
+  # params
+  #     none
+  # success: 
+  #     redirect to index if the user is logged in
+  # failure: 
+  #     none
   def check_login
     if logged_in?
       redirect_to :action => "index"
     end
   end
 
-  # returns true if the current user is logged in as admin
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     checks if the user is logged in
+  # params
+  #     none
+  # success: 
+  #     returns true if the user is logged in as admin
+  # failure: 
+  #     none
   def logged_in?
     !!current_user
   end
 
-  # returns the session variable who_is_this
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     current_user method
+  # params
+  #     none
+  # success: 
+  #     returns the session variable :who_is_this
+  # failure: 
+  #     none
   def current_user
     session[:who_is_this]
   end
 
-  # creates a session with variable who_is_this = "admin"
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     create session method
+  # params
+  #     none
+  # success: 
+  #     sets the session variable :who_is_this to "admin"
+  # failure: 
+  #     none
   def create_session
   	session[:who_is_this] = "admin"
   end
 
-  # clears the session variable who_is_this to nil
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     destroy_session method
+  # params
+  #     none
+  # success: 
+  #     unsets the session variable :who_is_this
+  # failure: 
+  #     none
   def destroy_session
   	session[:who_is_this] = nil
   end
@@ -47,13 +97,17 @@ class AdminController < ApplicationController
   def index
   end
 
-  # login action for admin
-  # @params:
-  #   username: the username for the admin
-  #   password: the password for the admin
-  # returns:
-  #   success: redirects to admin/index
-  #   failure: refreshes the page with error displayed
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     login action for admin
+  # params
+  #     username: the username for the admin
+  #     password: the password for the admin
+  # success: 
+  #     redirects to admin/index
+  # failure: 
+  #     refreshes the page with error displayed
   def login
     if request.post?
     	if params[:username] == "admin" and params[:password] == "admin"
@@ -65,13 +119,17 @@ class AdminController < ApplicationController
     	end
     end
   end
-
-  # wordadd action for keywords by admin
-  # @params:
-  #   name: the name of the new keyword
-  # returns:
-  #   success: refreshes the page and displays notification
-  #   failure: refreshes the page with error displayed
+  
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     wordadd action for keywords by admin
+  # params
+  #     name: the name of the new keyword
+  # success: 
+  #     refreshes the page and displays notification
+  # failure: 
+  #     refreshes the page with error displayed
   def wordadd
     name = params[:keyword][:name]
     is_english = params[:keyword][:is_english]
@@ -85,8 +143,16 @@ class AdminController < ApplicationController
     redirect_to :action => "index"
   end
 
-  # returns:
-  #   success: logout the user and redirect to admin login page
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     admin logout action
+  # params
+  #     none
+  # success: 
+  #     redirects the user to /admin/login page
+  # failure: 
+  #     none
   def logout
     destroy_session
     redirect_to :action => "login"
