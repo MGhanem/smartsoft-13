1b2fdf87c4b880cf3de6b5530d532d5f8cb81bff
Mirna Yacout <mirna.yacout@gmail.com>
2013-03-28 02:20:34 +0200

    indentation and specifying column name

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -2,21 +2,21 @@ class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
 
-	# Author:
-  #  Mirna Yacout
+  # Author:
+  # 	Mirna Yacout
   # Description:
-  #  This method is to record the aproval of the admin to a certain synonym in the database
+  # 	This method is to record the aproval of the admin to a certain synonym in the database
   # Parameters:
-  #  id: the id of the synonym to be approved
+  # 	id: the id of the synonym to be approved
   # Success:
-  #  returns true on saving the approval correctly in the database
+  # 	returns true on saving the approval correctly in the database
   # Failure:
-  #  returns false if the synonym doesnot exist in the database
-  #  or if the approval failed to be saved in the database 
-	class << self
-    def approve_synonym(id)
-      if (Synonym.exists?(id))
-        synonym = Synonym.find(id)
+  # 	returns false if the synonym doesnot exist in the database
+  # 	or if the approval failed to be saved in the database 
+  class << self
+    def approve_synonym(sid)
+      if (Synonym.exists?(id: sid))
+        synonym = Synonym.find(sid)
         synonym.approved = true
       return synonym.save
       end
