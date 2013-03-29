50c1076badce2df2848ce8ce3570724ed9af2fd8
nourhanZ <nourhan.nz@gmail.com>
2013-03-29 18:24:55 +0200

    [Issue #93] Finalized the comments and changed the UML according to the changed code

--- arability/app/models/vote.rb
+++ arability/app/models/vote.rb
@@ -2,11 +2,9 @@ class Vote < ActiveRecord::Base
   belongs_to :synonym
   belongs_to :gamer
   # attr_accessible :title, :body
-  validate :validate_gamer_exists, :validate_synonym_exists
-  validates :gamer_id, :uniqueness => { :scope => :synonym_id}
-
+ 
    # Author: Nourhan Zakaria
-   # This method is used to retreive a list of specific size of keywords 
+   # This method is used to get a list of specific size of keywords 
    # that gamer with this gamer_id didn't vote on yet.
    # Parameters:
    #  gamer_id: the gamer ID 
@@ -17,43 +15,31 @@ class Vote < ActiveRecord::Base
    # Returns:
    #  On success: Returns a list of un voted keywords of the specified langauge 
    #  with size = count for the gamer with this gamer_id  
-   #  On failure: Returns empty list or nil if the gamer with the input gamer_id doesn't exist
-  def self.get_unvoted_keywords(gamer_id, count, lang)
-      if Gamer.find_by_id(gamer_id) != nil
-        #voted_synonyms = Vote.where('gamer_id = ?', gamer_id).select('synonym_id')
-        #voted_keywords = Synonym.where(:id => voted_synonyms).select('keyword_id')
-        return Keyword.join(:synonym => [{:vote => :gamer .where('gamer_id=?', gamer_id)}] ).limit(count)
-
-          if lang == 0
-            un_voted_keywords = Keyword.where("is_english =?",true) - Keyword.where(:id => voted_keywords)
-          elsif lang ==1
-            un_voted_keywords = Keyword.where("is_english=?",false) - Keyword.where(:id => voted_keywords)
+   #  On failure: Returns empty list 
+class << self
+  def get_unvoted_keywords(gamer_id, count, lang)
+      unvoted_keywords = Keyword.where( "id Not IN(
+                                        Select K.id from Keywords K 
+                                        INNER JOIN Synonyms  S ON 
+                                        S.keyword_id = K.id AND S.id IN (
+                                        SELECT S1.id FROM synonyms S1 
+                                        INNER JOIN votes V on S1.id = 
+                                        V.synonym_id 
+                                        INNER JOIN gamers G on G.id = 
+                                        V.gamer_id AND G.id = ?)) 
+                                        OR id NOT IN( Select K1.id from 
+                                        Keywords K1 INNER JOIN Synonyms S1
+                                        ON S1.keyword_id = K1.id)", gamer_id)                            
+          if lang == 0    
+            keywords_list = unvoted_keywords.where('is_english=?', true)
+          elsif lang == 1
+            keywords_list = unvoted_keywords.where('is_english=?', false)
           else
-            un_voted_keywords = Keyword.all - Keyword.where( :id => voted_keywords) 
+            keywords_list = unvoted_keywords
           end
-        list_returned = un_voted_keywords.shuffle.sample(count)
-
-        return list_returned
-
-      else
-        return nil
-      end
+      return keywords_list.sample(count)    
   end
-
-  #This is a custom validation method that validates that there exists a gamer with this gamer_id 
-  def validate_gamer_exists
-    valid_gamer = Gamer.find_by_id(gamer_id)
-    if valid_gamer == nil
-      errors.add(:gamer_id,"this gamer_id soesn't exist")
-    end
-  end
-
-  #This is a custom validation method that validates that there exists a synonym with this synonym_id 
-  def validate_synonym_exists
-    valid_synonym = Synonym.find_by_id(synonym_id)
-    if valid_synonym == nil
-      errors.add(:synonym_id,"this synonym_id soesn't exist")
-    end
-  end
-
 end
+end
+
+  
