e611ba941ef899c67f72e037518ae0fea93f6e12
nourhanZ <nourhan.nz@gmail.com>
2013-03-29 19:56:55 +0200

    [Issue #93] added the comments on code

--- arability/app/models/vote.rb
+++ arability/app/models/vote.rb
@@ -4,27 +4,27 @@ class Vote < ActiveRecord::Base
   # attr_accessible :title, :body
   @@ENG = 0
   @@ARABIC = 1
- 
-  # Author: Nourhan Zakaria
-  # This method is used to get a list of specific size of keywords 
-  # that gamer with this gamer_id didn't vote on yet.
-  # Parameters:
-  #  gamer_id: the gamer ID 
-  #  count: the size of the list to be retreived
-  #  lang: integer to specify the language of keywords to be retreived  
-  #  if 0 then english only, if 1 then arabic only, 
-  #  otherwise both english and arabic keywords can be icluded
-  # Returns:
-  #  On success: Returns a list of un voted keywords of the specified langauge 
-  #  with size = count for the gamer with this gamer_id  
-  #  On failure: Returns empty list 
+  
   class << self
+    #Author: Nourhan Zakaria
+    #This method is used to get a list of specific size of keywords 
+    #that gamer with this gamer_id didn't vote on yet.
+    #Parameters:
+    #  gamer_id: the gamer ID 
+    #  count: the size of the list to be retreived
+    #  lang: integer to specify the language of keywords to be retreived  
+    #  if 0 then english only, if 1 then arabic only, 
+    #  otherwise both english and arabic keywords can be icluded
+    #Returns:
+    #  On success: Returns a list of un voted keywords of the specified langauge 
+    #  with size = count for the gamer with this gamer_id  
+    #  On failure: Returns empty list
     def get_unvoted_keywords(gamer_id, count, lang)
-      unvoted_keywords = Keyword.where( "id Not IN(
-        Select K.id from Keywords K INNER JOIN Synonyms  S ON S.keyword_id = 
-        K.id AND S.id IN ( SELECT S1.id FROM synonyms S1 INNER JOIN votes V 
-        on S1.id = V.synonym_id INNER JOIN gamers G on G.id = V.gamer_id 
-        AND G.id = ?)) OR id NOT IN( Select K1.id from Keywords K1 INNER JOIN 
+      unvoted_keywords = Keyword.where("id NOT IN(
+        SELECT K.id FROM Keywords K INNER JOIN Synonyms S ON S.keyword_id = 
+        K.id AND S.id IN (SELECT S1.id FROM synonyms S1 INNER JOIN votes V 
+        ON S1.id = V.synonym_id INNER JOIN gamers G ON G.id = V.gamer_id 
+        AND G.id = ?)) OR id NOT IN(SELECT K1.id FROM Keywords K1 INNER JOIN 
         Synonyms S1 ON S1.keyword_id = K1.id)", gamer_id)                            
       if lang == @@ENG    
         keywords_list = unvoted_keywords.where('is_english=?', true)
@@ -34,6 +34,26 @@ class Vote < ActiveRecord::Base
         keywords_list = unvoted_keywords
       end
       return keywords_list.sample(count)    
+    end
+
+    #Author: Nourhan Zakaria
+    #Returns the value of constant representing English language
+    #Parameters: --
+    #Returns:
+    #  On success: returns 0
+    #  On failure: --
+    def get_lang_english
+      return 0
+    end
+
+    #Author: Nourhan Zakaria
+    #Returns the value of constant representing Arabic language
+    #Parameters: --
+    #Returns:
+    #  On success: returns 1
+    #  On failure: --
+    def get_lang_arabic
+      return 1
     end
   end
 end
