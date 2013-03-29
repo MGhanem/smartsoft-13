761a35f869b3c665e839a4c12848c620cda4fdeb
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-03-29 16:03:53 +0200

    [issue #79][DONE] fixed 80 chars and code convetions

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -101,13 +101,13 @@ class Keyword < ActiveRecord::Base
     # params: 
     #   word: a Keyword to get the highest voted synonym for
     # return:
-    #   on succes: the highest voted Synonym of word gets returned. If two 
+    #   on success: the highest voted Synonym of word gets returned. If two 
     #               synonyms have an equal number of votes, the first synonym 
     #               entered to the list is returned.
     #   on failure: if word has no synonyms, nothing is returned
- 
-    def highest_voted_synonym(word)
-      max_id = Synonym.where(:keyword_id => word.id).joins(:votes).count(:group => "synonym_id").max
+    def highest_voted_synonym(keyword)
+      max_id = Synonym.where(:keyword_id => keyword.id).joins(:votes)
+      .count(:group => "synonym_id").max
       return Synonym.where(:id => max_id[0])
     end
 
