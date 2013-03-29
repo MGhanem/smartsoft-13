ab0913a180323eb49cd58026ee6ed91410425a85
Salma Farag Galal <salmafarag94@gmail.com>
2013-03-29 15:09:17 +0200

    Salma deleted a file

--- arability/db/migrate/20130329110632_change_categories_projects.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCategoriesProjects < ActiveRecord::Migration
-  def up 
-  	rename_column :categories_projects, :categories_id, :category_id
-  	rename_column :categories_projects, :projects_id, :project_id
-  end
-
-  def down
-  end
-end
