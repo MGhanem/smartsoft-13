5e815c44ff388a02ba3625a7873535acd9865037
Khloud Khalid <khloud.khalid7@gmail.com>
2013-03-29 18:57:04 +0200

    khloud fixed indentation

--- arability/app/controllers/developer_controller.rb
+++ arability/app/controllers/developer_controller.rb
@@ -1,50 +1,50 @@
 class DeveloperController < ApplicationController
 
-	before_filter :authenticate_gamer!
+  before_filter :authenticate_gamer!
 # author:
-# 	Khloud Khalid
+#   Khloud Khalid
 # description:
-#  	generates view with form for registration as developer
+#   generates view with form for registration as developer
 # params:
-#  	none
+#   none
 # success:
-#  	view generated successfully
+#   view generated successfully
 # failure:
-# 	gamer not signed in  	
-	def new
-		@developer = Developer.new
-	end
+#   gamer not signed in   
+  def new
+    @developer = Developer.new
+  end
 # author:
-# 	Khloud Khalid
+#   Khloud Khalid
 # description:
-#  	creates new developer using parameters from registration form and renders my_subscription#new
+#   creates new developer using parameters from registration form and renders my_subscription#new
 # params:
-#  	first_name, last_name
+#   first_name, last_name
 # success:
-#  	developer created successfully
+#   developer created successfully
 # failure:
-# 	invalid information, user already registered as developer
-	def create
-		@developer = Developer.new(params[:developer])
-		@developer.gamer_id = current_gamer.id
-		if @developer.valid?
-			if @developer.save 
-				render 'my_subscription/new'
-			else
-				flash[:notice] = "Failed to complete registration."
-				render :action => 'new'
-			end
-				
-		else
-			if((Developer.find_by_gamer_id(current_gamer.id)) != nil)
-				flash[:notice] = "You are already registered as a developer."	
-				render :action => 'new'
-			else
-				flash[:notice] = "Please make sure you entered valid information."	
-				render :action => 'new'	
-			end		
-		end	
-	end
+#   invalid information, user already registered as developer
+  def create
+    @developer = Developer.new(params[:developer])
+    @developer.gamer_id = current_gamer.id
+    if @developer.valid?
+      if @developer.save 
+        render 'my_subscription/new'
+      else
+        flash[:notice] = "Failed to complete registration."
+        render :action => 'new'
+      end
+        
+    else
+      if((Developer.find_by_gamer_id(current_gamer.id)) != nil)
+        flash[:notice] = "You are already registered as a developer." 
+        render :action => 'new'
+      else
+        flash[:notice] = "Please make sure you entered valid information."  
+        render :action => 'new' 
+      end   
+    end 
+  end
 end
 
 
--- arability/app/controllers/my_subscription_controller.rb
+++ arability/app/controllers/my_subscription_controller.rb
@@ -1,48 +1,48 @@
 class MySubscriptionController < ApplicationController
-	before_filter :authenticate_gamer!
+    before_filter :authenticate_gamer!
 # author:
-# 	Khloud Khalid
+#   Khloud Khalid
 # description:
-#  	generates view with form for choosing subscription model
+#   generates view with form for choosing subscription model
 # params:
-#  	none
+#   none
 # success:
-#  	view generated successfully
+#   view generated successfully
 # failure:
-# 	gamer not signed in  
-	def new
-			@my_subscription = MySubscription.new
-	end
+#   gamer not signed in  
+    def new
+            @my_subscription = MySubscription.new
+    end
 # author:
-# 	Khloud Khalid
+#   Khloud Khalid
 # description:
-#  	creates new my_subscription using parameters from registration form and links it to the developer
+#   creates new my_subscription using parameters from registration form and links it to the developer
 # params:
-#  	subscription_model_id
+#   subscription_model_id
 # success:
-#  	my_subscription created successfully and linked to developer
+#   my_subscription created successfully and linked to developer
 # failure:
-# 	invalid information
-	def create
-		if(SubscriptionModel.find_by_id(params[:my_subscription]) != nil)
-			@my_subscription = MySubscription.new
-			@my_subscription.developer_id = Developer.find_by_gamer_id(current_gamer.id).id
-			@my_subscription.subscription_models_id = params[:my_subscription]
-			if @my_subscription.save 
-				flash[:notice] = "You have successfully registered as a developer."
-				render 'pages/home'
-			else
-				flash[:notice] = "Failed to complete registration."
-				render 'my_subscription/new'
-			end
-		else
-			if(params[:my_subscription] == nil)
-				flash[:notice] = "Please choose a subscription model."
-				render 'my_subscription/new'
-			else
-				flash[:notice] = "Failed to complete registration: the subscription model you chose does not exist."
-				render 'my_subscription/new'
-			end
-		end
-	end	
+#   invalid information
+    def create
+        if(SubscriptionModel.find_by_id(params[:my_subscription]) != nil)
+            @my_subscription = MySubscription.new
+            @my_subscription.developer_id = Developer.find_by_gamer_id(current_gamer.id).id
+            @my_subscription.subscription_models_id = params[:my_subscription]
+            if @my_subscription.save 
+                flash[:notice] = "You have successfully registered as a developer."
+                render 'pages/home'
+            else
+                flash[:notice] = "Failed to complete registration."
+                render 'my_subscription/new'
+            end
+        else
+            if(params[:my_subscription] == nil)
+                flash[:notice] = "Please choose a subscription model."
+                render 'my_subscription/new'
+            else
+                flash[:notice] = "Failed to complete registration: the subscription model you chose does not exist."
+                render 'my_subscription/new'
+            end
+        end
+    end 
 end
