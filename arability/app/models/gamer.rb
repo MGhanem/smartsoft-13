71231260b70673103ffb62335024c0c28f1946a8
Kareem Ali <kareemali@vpm2.pao1.sonic.net>
2013-04-01 11:06:10 +0200

    #86 replacing text for select to bdate in sign up page

--- arability/app/models/gamer.rb
+++ arability/app/models/gamer.rb
@@ -6,16 +6,14 @@ class Gamer < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, 
-                  :username, :age, :country, :education_level
+                  :username, :country, :education_level, :date_of_birth
                   
-  # attr_accessible :title, :body
-  validates :username, :presence => true, :length => { :minimum => 2 }
-  validates :username, :format => { :with => /\A[a-zA-Z]+\z/,:message => "can't be anything except letters." }
-  validates :age, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100, :only_integer => true }
+                  
+  validates :username, :presence => true, :length => { :minimum => 3 }
+  validates :username, :format => { :with => /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/i,:message => " can only start with a letter, and have no 2 successive \"_\"" }
   validates :country, :presence => true, :length => { :minimum => 2 }
   validates :country, :format => { :with => /\A[a-zA-Z]+\z/,:message => "can't be anything except letters." }
-  validates :education_level, :presence => true, :length => { :minimum => 2 }
-  validates :education_level, :format => { :with => /\A^(low|medium|high)\Z/i,:message => "can't be except low, medium or high" }
+  validates :education_level, :format => { :with => /\A^(low|medium|high)\Z/i }
 
 
 
