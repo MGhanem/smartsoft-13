571cd734e903c6476884683605c78f2a5b6facdf
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-01 17:54:42 +0200

    implementation has been remodified[issue #89]

--- arability/app/models/keyword.rb
+++ arability/app/models/keyword.rb
@@ -31,21 +31,18 @@ class Keyword < ActiveRecord::Base
       return false
     end
 
-# author:
-#   Omar Hossam
-# description:
-#   feature takes no input and returns a list of all unapproved keywords
-# success: 
-#   takes no arguments and returns to the admin a list containing the keywords 
-#   that are pending for approval in the database
-# failure:
-#   returns an empty list if no words are pending for approval
-
-  def listunapprovedkeywords
-
-    return Keyword.where(approved: false).all
-
-  end
+  # author:
+  #   Omar Hossam
+  # description:
+  #   feature takes no input and returns a list of all unapproved keywords
+  # success: 
+  #   takes no arguments and returns to the admin a list containing the keywords
+  #   that are pending for approval in the database
+  # failure:
+  #   returns an empty list if no words are pending for approval
+    def list_unapproved_keywords
+      return Keyword.where(approved: false).all
+    end
 
     # adds a new keyword to the database
     # author:
