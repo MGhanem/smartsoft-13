1ad31553e5e367fb43b2239c4aef853c7371a151
Adam G <ghanem.adam@gmail.com>
2013-04-01 18:38:31 +0200

    created many to many relation table between gamer and prizes and gamer and trophies

---
+++ arability/db/migrate/20130401163655_create_gamer_prizes.rb
@@ -0,0 +1,12 @@
+class CreateGamerPrizes < ActiveRecord::Migration
+  def up
+    create_table :gamers_prizes, :id => false do |t|
+      t.references :gamer
+      t.references :trophy
+    end
+  end
+
+  def down
+    drop_table :gamers_prizes
+  end
+end
