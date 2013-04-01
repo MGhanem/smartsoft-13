b878d2d6c4ecf07130008fb38008d18bc458eb72
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-04-01 11:29:46 +0200

    [issue #172][WIP]commiting for checkout to migration

--- arability/app/models/follow.rb
+++ arability/app/models/follow.rb
@@ -1,3 +1,10 @@
 class Follow < ActiveRecord::Base
   attr_accessible :developer_id, :keyword_id
+  # validates :developer_id, :keyword_id, :presence_of => true
+  class << self
+    
+    # def follow(developer, keyword)
+    #   follows = self.new(:developer_id => developer.id, :keyword_id => keyword.id)
+    # end
+  end
 end
