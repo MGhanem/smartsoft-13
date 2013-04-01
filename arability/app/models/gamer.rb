22511b7e02397f13b3429837d96bcdc5fb383b2a
Kareem Ali <kareemali@vpm2.pao1.sonic.net>
2013-04-01 15:03:27 +0200

    #86 applying some code styling conventions in gamer model

--- arability/app/models/gamer.rb
+++ arability/app/models/gamer.rb
@@ -10,10 +10,19 @@ class Gamer < ActiveRecord::Base
                   
 
   validates :username, :presence => true, :length => { :minimum => 3 }
-  validates :username, :format => { :with => /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/i,:message => " can only start with a letter, and have no 2 successive \"_\"" }
+
+  validates :username, :format => { :with => /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/i, 
+    :message => " can only start with a letter, 
+    and have no 2 successive \"_\"" }
+
   validates :country, :presence => true, :length => { :minimum => 2 }
-  validates :country, :format => { :with => /\A[a-zA-Z]+\z/,:message => "can't be anything except letters." }
+
+  validates :country, :format => { :with => /\A[a-zA-Z]+\z/,
+    :message => "can't be anything except letters." }
+
   validates :education_level, :format => { :with => /\A^(low|medium|high)\Z/i }
-  validates :date_of_birth, :date => { :after_or_equal_to => Time.now-95.year, :before_or_equal_to => Time.now - 10.year }
+  
+  validates :date_of_birth, :date => { :after_or_equal_to => 95.years.ago, 
+    :before_or_equal_to => 10.years.ago }
 
 end
