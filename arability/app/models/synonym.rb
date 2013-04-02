a99c74b6a423e891143ec8deb92fbfea21c00f8d
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-04-02 11:53:49 +0200

    [issue #186] [WIP] deleted method fnd from synonym model

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -2,8 +2,8 @@ class Synonym < ActiveRecord::Base
   belongs_to :keyword
   attr_accessible :approved, :name
   has_many :votes
-  validates_format_of :name, :with => /^([\u0621-\u0652 ])+$/,
-    :message => "The synonym is not arabic"
+  #validates_format_of :name, :with => /^([\u0621-\u0652 ])+$/,
+    #:message => "The synonym is not arabic"
 
   class << self
 
@@ -59,11 +59,6 @@ class Synonym < ActiveRecord::Base
       else
         return false
       end
-    end
-  end
-
-    def find(syn, key_id)
-      return Synonym.where("keyword_id = ? AND name = ?", key_id, syn)  
     end
   end
 end
