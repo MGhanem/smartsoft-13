1a2c992058a02a1b6c1f2f608318812065c044b0
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-04-03 17:18:34 +0200

    commiting for checkout

--- arability/app/models/follow.rb
+++ arability/app/models/follow.rb
@@ -1,10 +1,4 @@
 class Follow < ActiveRecord::Base
   attr_accessible :developer_id, :keyword_id
-  # validates :developer_id, :keyword_id, :presence_of => true
-  class << self
-    
-    # def follow(developer, keyword)
-    #   follows = self.new(:developer_id => developer.id, :keyword_id => keyword.id)
-    # end
-  end
-end
+  validates :developer_id, :keyword_id, :presence_of => true
+ end
