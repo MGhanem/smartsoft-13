6ef2fafab1e376aaf5defbe920ab596cdd796d7f
Noha Hesham <nohahesham16@gmail.com>
2013-04-04 17:12:38 +0200

    commiting the migration

--- arability/db/migrate/20130403200432_create_shared_projects.rb
+++ arability/db/migrate/20130403200432_create_shared_projects.rb
@@ -1,6 +1,7 @@
 class CreateSharedProjects < ActiveRecord::Migration
   def change
-    create_table :shared_projects do |t|
+  	drop_table :developers_projects
+    create_table :shared_projects ,:id => false do |t|
       t.integer :project_id
       t.integer :developer_id
       t.timestamps
