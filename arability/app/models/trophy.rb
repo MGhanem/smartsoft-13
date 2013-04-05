6067f254e51fee76da0602cc4a47f7b1e1d178ac
karim el naggar <karimfarid.naggar@gmail.com>
2013-04-05 20:03:39 +0200

    issue [#173] [WIP] fixed a bug in trophy model

--- arability/app/models/trophy.rb
+++ arability/app/models/trophy.rb
@@ -1,19 +1,19 @@
 class Trophy < ActiveRecord::Base
-  
+  include Paperclip::Glue
   validates :name, :presence => true, :length => { :in => 6..24 },
     :uniqueness => true
   validates :level, :presence => true, :numericality => {
     :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100 }
   validates :score, :presence => true, :numericality => {
     :greater_than_or_equal_to => 1, :less_than_or_equal_to => 1000000 }
-  validates :photo, :presence => true
-  validates_attachment_size :photo, :in => 0.megabytes..0.5.megabytes
+  validates :image, :presence => true
+  validates_attachment_size :image, :in => 0.megabytes..0.5.megabytes
   
   has_and_belongs_to_many :gamers
 
-  attr_accessible :name, :level, :score, :photo
+  attr_accessible :name, :level, :score, :image
 
-  has_attached_file :photo
+  has_attached_file :image
 
   class << self
 
@@ -22,13 +22,13 @@ class Trophy < ActiveRecord::Base
     # description:
     #     a function adds a new trophy to the database
     # params
-    #     name, score, rank, photo
+    #     name, score, rank, image
     # success: 
     #     returns true and the new trophy if it is added to the database
     # failure: 
     #     returns false and the trophy if it is not added to the database
     def add_trophy_to_database(name, score, rank, photo)
-      new_trophy = Trophy.new(name: name, score: score, level: rank, photo: photo)
+      new_trophy = Trophy.new(name: name, score: score, level: rank, image: photo)
       if new_trophy.save
         return true, new_trophy
       else
