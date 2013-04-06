702c06e71a9a7d15375689b6a7d994ca80496b5c
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-04-06 12:13:05 +0200

    [issue #167][NW] created initial function in search helper

--- arability/app/helpers/search_helper.rb
+++ arability/app/helpers/search_helper.rb
@@ -1,2 +1,39 @@
 module SearchHelper
+
+  def chart
+    chart = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Browser share',
+               :data=> [
+                  ['Firefox',   45.0],
+                  ['IE',       26.8],
+                  {
+                     :name=> 'Chrome',    
+                     :y=> 12.8,
+                     :sliced=> true,
+                     :selected=> true
+                  },
+                  ['Safari',    8.5],
+                  ['Opera',     6.2],
+                  ['Others',   0.7]
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = "THA PIE"
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+    end
+  end
 end
