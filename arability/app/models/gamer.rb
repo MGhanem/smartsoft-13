7fae4908ac06e87cb6d1bf2e69451d9007a5a984
Salma Farag Galal <salmafarag94@gmail.com>
2013-04-07 17:10:42 +0200

    [208][WIP] Salma added Google related attributes in Gamer

--- arability/app/models/gamer.rb
+++ arability/app/models/gamer.rb
@@ -2,8 +2,8 @@ class Gamer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         # :omniauthable, :omniauth_providers => [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, 
