0024ca53ed491af4886e52ee02428c9d8cc389f9
Salma Farag Galal <salmafarag94@gmail.com>
2013-04-07 13:13:10 +0200

    [Issue#208][WIP] Salma added attributes in Gamer

--- arability/app/models/gamer.rb
+++ arability/app/models/gamer.rb
@@ -7,7 +7,8 @@ class Gamer < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, 
-                  :username, :country, :education_level, :date_of_birth
+                  :username, :country, :education_level, :date_of_birth,
+                  :provider, :uid
 
   has_many :services, :dependent => :destroy
                   
