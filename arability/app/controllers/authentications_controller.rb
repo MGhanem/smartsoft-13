c4ef994ca78b83d0eaf1207e4795868608459b19
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-07 19:40:52 +0200

    [#181][WIP] Twitter is written in arabic in all views

--- arability/app/controllers/authentications_controller.rb
+++ arability/app/controllers/authentications_controller.rb
@@ -5,13 +5,13 @@ class AuthenticationsController < ApplicationController
 	end
 
 	def twitter_callback
-	  flash[:notice] = "Twitter تم الاتصال مع"
+	  flash[:notice] = "تم الاتصال مع تويتر"
 	  auth = request.env["omniauth.auth"]
       authentication = Authentication.find_by_provider_and_gid(auth["provider"], auth["gid"]) || Authentication.create_with_omniauth(auth)
 	end
 
 	def remove_twitter_connection
-	  flash[:notice] = "Twitter تم إلغاء الاتصال مع"
+	  flash[:notice] = "تم إلغاء الاتصال مع تويتر"
 	  Authentication.find_by_gamer_id_and_provider(current_gamer, "Twitter").destroy
 	  render :action => "twitter"
 	end
