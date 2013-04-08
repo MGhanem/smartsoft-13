3df92935c15235fc629cb418591f6f7311fc99ae
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-04-08 22:15:51 +0200

    Changed error message

--- arability/app/controllers/pages_controller.rb
+++ arability/app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def routing_error
-    redirect_to "/", flash: {error: "Sorry, you have tried to access a non existing page \"#{params[:path]}\". You have been redirected to the home page"}
+    redirect_to "/", flash: {error: "Sorry, you have tried to access a non existing page \"#{params[:path]}\". An admin has been notified of your error"}
   end
 end
