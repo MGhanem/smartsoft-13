68c144f0b66995581ce00ec09b2f1d7705b234ee
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2013-04-08 23:17:45 +0200

    [issue #177][WIP] fixing bugs in follow buttons

--- arability/app/controllers/follow_controller.rb
+++ arability/app/controllers/follow_controller.rb
@@ -5,11 +5,11 @@ class FollowController < BackendController
     developer = Developer.where(:gamer_id => current_gamer.id).first
     keyword_ids = developer.keyword_ids
     word = Keyword.find(params[:keyword_id]).name
-    if keyword_ids.include? params[:keyword_id]
+    if keyword_ids.include? params[:keyword_id].to_i
+      redirect_to :search, :flash => {:fail => "#{t(:follow_keyword_alert_fail)} #{word}"}
+    else
       developer.follow(params[:keyword_id])
       redirect_to :search, :flash => {:success => "#{t(:follow_keyword_alert)} #{word}"}
-    else
-      redirect_to :search, :flash => {:fail => "#{t(:follow_keyword_alert_fail)} #{word}"}
     end
   end
 
