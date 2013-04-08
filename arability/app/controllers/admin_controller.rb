2af3a6080ae77e04aa47e45c91cc268995778825
karim el naggar <karimfarid.naggar@gmail.com>
2013-04-08 18:23:53 +0200

    move admin session handlers to AdminHelper

--- arability/app/controllers/admin_controller.rb
+++ arability/app/controllers/admin_controller.rb
@@ -4,8 +4,10 @@ class AdminController < ApplicationController
 
   layout 'admin'
 
+  include AdminHelper
+
   before_filter :require_login
-  skip_before_filter :require_login, only: [:login]
+  skip_before_filter :require_login, only: [:login, :logout]
 
   before_filter :check_login, only: [:login]
 
@@ -40,62 +42,6 @@ class AdminController < ApplicationController
     if logged_in?
       redirect_to action: "index"
     end
-  end
-
-  # author:
-  #     Karim ElNaggar
-  # description:
-  #     checks if the user is logged in
-  # params
-  #     none
-  # success: 
-  #     returns true if the user is logged in as admin
-  # failure: 
-  #     none
-  def logged_in?
-    current_user == "admin"
-  end
-
-  # author:
-  #     Karim ElNaggar
-  # description:
-  #     current_user method
-  # params
-  #     none
-  # success: 
-  #     returns the session variable :who_is_this
-  # failure: 
-  #     none
-  def current_user
-    session[:who_is_this]
-  end
-
-  # author:
-  #     Karim ElNaggar
-  # description:
-  #     create session method
-  # params
-  #     none
-  # success: 
-  #     sets the session variable :who_is_this to "admin"
-  # failure: 
-  #     none
-  def create_session
-    session[:who_is_this] = "admin"
-  end
-
-  # author:
-  #     Karim ElNaggar
-  # description:
-  #     destroy_session method
-  # params
-  #     none
-  # success: 
-  #     unsets the session variable :who_is_this
-  # failure: 
-  #     none
-  def destroy_session
-    session[:who_is_this] = nil
   end
 
   def index
