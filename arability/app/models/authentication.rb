99ab611c6cf164adc734d707033b65746e3f4217
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-08 21:11:17 +0200

    [issue #199][WIP] created branch for story

--- arability/app/models/authentication.rb
+++ arability/app/models/authentication.rb
@@ -1,3 +1,14 @@
 class Authentication < ActiveRecord::Base
   attr_accessible :gid, :provider, :token, :token_secret
+  require 'open-uri'
+  require 'json'
+
+  class << self
+
+    def common_twitter_followers(gid)
+  	  result = JSON.parse("https://api.twitter.com/1/followers/ids.json?user_id=:gid&screen_name=twitterapi")
+    end
+
+  end
+
 end
