c6d9e627aefebc4abbd114aba9b8953ffb3de618
Khloud Khalid <khloud.khalid7@gmail.com>
2013-04-08 21:40:36 +0200

    [Issue #85] [WIP] changes regex validation of developer to accept arabic letters

--- arability/app/models/developer.rb
+++ arability/app/models/developer.rb
@@ -3,8 +3,8 @@ class Developer < ActiveRecord::Base
  attr_accessible :first_name, :last_name, :verified, :gamer_id
  validates :first_name, :presence => true
  validates :last_name, :presence => true
- validates_format_of :first_name, :with => /\A[a-zA-Z]+\z/
- validates_format_of :last_name, :with => /\A[a-zA-Z]+\z/
+ validates_format_of :first_name, :with => /^([\u0621-\u0652 ]+|[a-zA-z ]+)$/
+ validates_format_of :last_name, :with => /^([\u0621-\u0652 ]+|[a-zA-z ]+)$/
  validates_length_of :first_name, :maximum => 18
  validates_length_of :first_name, :minimum => 3
  validates_length_of :last_name, :maximum => 18
