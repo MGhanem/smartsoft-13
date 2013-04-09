cbf1bacd7b693b85e3f3ec35b875a8aa12698d83
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-09 18:02:47 +0200

    [Issue #195][WIP] added search_keyword method to handle searching for keywords

--- arability/app/controllers/search_controller.rb
+++ arability/app/controllers/search_controller.rb
@@ -1,5 +1,37 @@
 class SearchController < ApplicationController
   before_filter :authenticate_gamer!
+  #Description:
+  #   search for keywords (in a particular category)
+  # Author:
+  #   Mohamed Ashraf, Nourhan Mohamed
+  # params:
+  #   search: a string representing the search keyword, from the params list
+  #     from a textbox in the search view
+  #   categories: [optional] a string by which the categories can be filtered
+  #   is_successful: [optional] a boolean that is passed and set to true if the
+  #     controller is accessed due to a redirect from the keyword controller
+  # returns:
+  #   success: 
+  #     returns to the search view a list of keywords (in a certain category)
+  #     similar to the search keyword sorted by relevance
+  #   failure:
+  #     returns an empty list if the search keyword had no matches or no
+  #     similar keywords were found
+  def search_keywords
+    @categories = params[:categories]
+    if @categories.present?
+      categories_array = @categories.split(/,/)
+      categories_array.reject! { |x| x.blank? }
+      categories_array.map! { |x| x.strip }
+      categories_array.uniq!
+    else
+      categories_array = []
+    end
+    @search_keyword = params['search']
+    @similar_keywords =
+      Keyword.get_similar_keywords(@search_keyword, categories_array)
+  end
+
 	#Description:
   #   search for synonyms for a particular keyword
   # Author:
@@ -30,9 +62,6 @@ class SearchController < ApplicationController
       @age_to = temp
     end
     @gender = params['gender']
-    if(@search_keyword.blank? && @search_keyword_model.blank?)
-      @display_add = false
-    end
     if(!@search_keyword.blank?)
       @no_synonyms_found = false
       @search_keyword_model = Keyword.find_by_name(@search_keyword)
@@ -47,8 +76,11 @@ class SearchController < ApplicationController
           @total_votes += synonym_votes
         end
       else
-        @display_add = true
+        redirect_to search_keywords_path(:search => @search_keyword)
       end
+    else
+      flash = { :warning => 'Search Keyword can\'t be empty' }
+      redirect_to search_keywords_path, :flash => flash
     end
   end
 end
