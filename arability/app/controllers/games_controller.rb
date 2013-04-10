2bc076349c09df6fbea8301ba9227b515280f32d
Adam G <ghanem.adam@gmail.com>
2013-04-10 09:40:16 +0200

    [WIP] More refacoring, arabization and documented controller action

--- arability/app/controllers/games_controller.rb
+++ arability/app/controllers/games_controller.rb
@@ -8,11 +8,23 @@ class GamesController < ApplicationController
   def record_vote
   end
 
-
+  # Description:
+  #   After the gamer finishes a level this action is requested
+  #   to award them with their prizes if they have any
+  # Author:
+  #   Adam Ghanem
+  # @params:
+  #   level: the level that the gamer finishes
+  #   score: the score that the gamer earns
+  # returns:
+  #   success: lists out the prizes the gamer wins in a js erb view
+  #   failure: the action is not even called
   def getprizes
     @level = params[:level].to_i
     @score = params[:score].to_i
-    @won_prizes = Prize.get_new_prizes_for_gamer(current_gamer.id, @score, @level)
+    @won_prizes = Prize.get_new_prizes_for_gamer(current_gamer.id, 
+                                                 @score, @level)
+    @won_prizes.map { |wp| current_gamer.prizes << wp }
     respond_to do |format|
       format.js
     end
