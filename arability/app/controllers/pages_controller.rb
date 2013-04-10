3b5b8f94cc57e920ec8d95608f4349d54bbaff9d
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-10 15:01:20 +0200

    [WIP][issue #178] documentation written

--- arability/app/controllers/pages_controller.rb
+++ arability/app/controllers/pages_controller.rb
@@ -3,12 +3,23 @@ class PagesController < ApplicationController
 
   def home
   end
-
-  def post
-    token = Gamer.fetToken(current_gamer.id)
-	  @graph = Koala::Facebook::API.new(token)
-	  @graph.put_wall_post("لقد حصلت على ١٢٣ نقطة في عربيلتي")
-	  render 'pages/home'
-  end
+  # Author:
+  #   Omar Hossam
+  # Description:
+  #   As a gamer, I could post my score on my facebook timeline by pressing on
+  #   the facebook share score button.
+  # Parameters:
+  #   None.
+  # Success:
+  #   Gamer presser the facebook share score button, and his score is shared on
+  #   facebook and confirmed by API.
+  # Failure: 
+  #   Facebook failure reported by API.
+    def post
+      token = Gamer.fetToken(current_gamer.id)
+      @graph = Koala::Facebook::API.new(token)
+      @graph.put_wall_post("لقد حصلت على ١٢٣ نقطة في عربيلتي")
+      render 'pages/home'
+    end
 
 end
