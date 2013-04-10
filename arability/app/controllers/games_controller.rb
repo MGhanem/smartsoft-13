3a630f45577865275ac2760d28783060fbb34ff1
Adam G <ghanem.adam@gmail.com>
2013-04-10 16:48:30 +0200

    [WIP] Arabized views

--- arability/app/controllers/games_controller.rb
+++ arability/app/controllers/games_controller.rb
@@ -10,8 +10,23 @@ class GamesController < ApplicationController
   end
   
 
-
+  # Description:
+  #   Returns the view that the gamer can view the prizes that 
+  #   are available currently in the game and the prizes that they
+  #   have already earned
+  # Author:
+  #   Adam Ghanem
+  # @params:
+  #   none
+  # returns:
+  #   success: renders out a view using js erb view with the 
+  #   earned prizes in a list and the prizes that haven't been 
+  #   earned in another
   def showprizes
     @won_prizes = current_gamer.get_won_prizes
+    @not_won_prizes = current_gamer.get_available_prizes
+    respond_to do |format|
+      format.js
+    end
   end
 end
