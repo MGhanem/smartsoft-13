b93ba1699682e9b181a366680b7e7e706d39c2c8
Amr Hatem Abdelraouf <amr.abdelraouf92@gmail.com>
2013-04-10 13:24:52 +0200

    [ISSUE #180][WIP] reverts my deleted commit 2

--- arability/app/controllers/games_controller.rb
+++ arability/app/controllers/games_controller.rb
@@ -8,10 +8,5 @@ class GamesController < ApplicationController
   def record_vote
   end
 
-  def post
-    token = Gamer.getToken(current_gamer.id)
-    @graph = Koala::Facebook::API.new(token)
-    @graph.put_wall_post("Checkout the new Arability game @ www.arability.net")
-    render '/game'
-  end
+
 end
