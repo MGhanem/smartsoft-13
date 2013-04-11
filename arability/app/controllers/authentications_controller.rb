4037d03fbb5b442211ab6137937e6f7a74d57994
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-11 21:20:20 +0200

    [#199][WIP]

--- arability/app/controllers/authentications_controller.rb
+++ arability/app/controllers/authentications_controller.rb
@@ -80,16 +80,4 @@ class AuthenticationsController < ApplicationController
 	  redirect_to root_url
 	end
 
-	def twitter_hall_of_fame
-		if Authentication.get_common_twitter_followers(current_gamer).nil?
-		  if I18n.locale==:en
-	  	    flash[:notice] = "You've not connected your account to twitter"
-	  	  else I18n.locale==:ar
-	  	    flash[:notice] = "لم يتم التواصل بتويتر"
-	  	  end
-		  render :action => "twitter"
-		  return
-		end
-	end
-
 end
