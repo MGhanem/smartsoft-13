27e80a49507a7767d636b257d88b21a4ae60d8f0
karim el naggar <karimfarid.naggar@gmail.com>
2013-04-11 23:14:20 +0200

    edit tests

--- arability/spec/models/trophy_spec.rb
+++ arability/spec/models/trophy_spec.rb
@@ -3,8 +3,18 @@ require "spec_helper"
 
 describe Trophy do
 
+	it "should not be saved" do
+		trophy = Trophy.new
+		expect(trophy.save).to eq(false)
+	end
+
 	it "should return false since name and image is empty" do
-		success, trophy = Trophy.add_trophy_to_database("", 1, 1, nil)
+		success, trophy = Trophy.add_prize_to_database(nil, 1, 1, nil)
+		expect(success).to eq(false)
+	end
+
+	it "should return false since name and score and level and image is empty" do
+		success, trophy = Trophy.add_prize_to_database(nil, nil, nil, nil)
 		expect(success).to eq(false)
 	end
 
