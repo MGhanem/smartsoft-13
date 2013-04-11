7240e4313ed936af170bc5ecb0c676e3ac8a1ffc
Noha Hesham <nohahesham16@gmail.com>
2013-04-11 23:47:36 +0200

    fixed migration

--- arability/db/migrate/20130411191248_change_subscription_model_relation.rb
+++ arability/db/migrate/20130411191248_change_subscription_model_relation.rb
@@ -1,7 +1,7 @@
 class ChangeSubscriptionModelRelation < ActiveRecord::Migration
   def up
   	remove_column :my_subscriptions, :subscription_models_id
-  	add_column :my_subscriptions, :subscription_model_id
+  	add_column :my_subscriptions, :subscription_model_id, :integer
   end
 
   def down
