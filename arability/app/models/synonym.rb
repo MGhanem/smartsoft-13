75cf93c534fcf86ac0b6bebf6fdfd9720b545947
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-11 21:15:34 +0200

    [WIP][issue #83] code finished

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -37,37 +37,38 @@ class Synonym < ActiveRecord::Base
       return false
     end
 
-  # author:
-  #   Omar Hossam
-  # description:
-  #   feature adds synonym to database and returns a boolean result
-  #   indicatiing success or failure of saving
-  # parameters:
-  #   synonym_name: string input parameter that represents the synonym name
-  #   keyword_id: integer input parameter representing the keyword id
-  #     the synonym points to
-  #   approved: an optional boolean input parameter with a default false
-  #     represents if an admin has approved a synonym on database or not
-  # success:
-  #   Output is boolean -- this method returns true if
-  #     the vote has been recorded
-  # failure: 
-  #   returns false if word not saved to database due to incorrect expression
-  #   of synonym name or an incorrect keyword id for
-  #   an unavaialable keyword in database
-    def record_synonym(synonym_name, keyword_id, approved = false)
-      if synonym_name.blank?
-        return false
-      elsif Synonym.exists?(name: synonym_name, keyword_id: keyword_id)
-        return false
-      elsif Keyword.exists?(id: keyword_id)
-        new_synonym = Synonym.new
-        new_synonym.name = synonym_name
-        new_synonym.keyword_id = keyword_id
-        return new_synonym.save
-      else
-        return false
-      end
+  # Author:
+  #  Omar Hossam
+  # Description:
+  #  This is the modified function of "recordsynonym". Feature adds synonym to
+  #  database and returns a boolean result
+  #  indicatiing success or failure of saving.
+  # Parameters:
+  #  synonym_name: string input parameter that represents the synonym name.
+  #  keyword_id: integer input parameter representing the keyword id
+  #    the synonym points to.
+  #  approved: an optional boolean input parameter with a default false
+  #    represents if an admin has approved a synonym on database or not.
+  # Success:
+  #  Output is boolean -- this method returns true if
+  #    the vote has been recorded.
+  # Failure: 
+  #  returns false if word not saved to database due to incorrect expression
+  #  of synonym name or an incorrect keyword id for
+  #  an unavaialable keyword in database.
+  def record_synonym(synonym_name, keyword_id, approved = false)
+    if synonym_name.blank?
+      return false
+    elsif Synonym.exists?(name: synonym_name, keyword_id: keyword_id)
+      return false
+    elsif Keyword.exists?(id: keyword_id)
+      new_synonym = Synonym.new
+      new_synonym.name = synonym_name
+      new_synonym.keyword_id = keyword_id
+      return new_synonym.save
+    else
+      return false
     end
+  end
   end
 end
