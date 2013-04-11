2930b2f438ccf2b1ca3464460b3b5837346c4455
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-11 21:23:34 +0200

    [#199][WIP] changed the get statement in routes

--- arability/config/routes.rb
+++ arability/config/routes.rb
@@ -180,7 +180,7 @@ Arability::Application.routes.draw do
   match '/auth/:twitter/callback', :to => 'authentications#twitter_callback' 
   match '/auth/failure', :to => 'authentications#twitter_failure'
  
-  get "authentications/twitter_hall_of_fame"
+  get "games/halloffame"
 
   get "/en/gamers" => redirect('/en/gamers/sign_up')
 
