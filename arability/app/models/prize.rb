f627a0d54e1de27ec491d468d8ca305b130d4e87
Adam G <ghanem.adam@gmail.com>
2013-04-12 17:30:36 +0200

    [Issue #164][WIP] documented prize method

--- arability/app/models/prize.rb
+++ arability/app/models/prize.rb
@@ -1,30 +1,45 @@
 class Prize < ActiveRecord::Base
-  
-  validates :name, :presence => true, :length => { :in => 6..24 }, 
-    :uniqueness => true
-  validates_format_of :name, :with => /^([\u0621-\u0652 ])+$/
-  validates :level, :presence => true, :numericality => { 
-    :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100 }
-  validates :score, :presence => true, :numericality => { 
-    :greater_than_or_equal_to => 1, :less_than_or_equal_to => 1000000 }
-                              # This will probably be changed when we figure
-                              # the scoring system the game will have
-  # validates :photo, :presence => true
-  # validates_attachment_size :photo, :in => 0.megabytes..0.5.megabytes
-  
-  has_and_belongs_to_many :gamers
+    
+    validates :name, :presence => true, :length => { :in => 6..24 }, 
+      :uniqueness => true
+    validates_format_of :name, :with => /^([\u0621-\u0652 ])+$/
+    validates :level, :presence => true, :numericality => { 
+      :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100 }
+    validates :score, :presence => true, :numericality => { 
+      :greater_than_or_equal_to => 1, :less_than_or_equal_to => 1000000 }
+                                # This will probably be changed when we figure
+                                # the scoring system the game will have
+    # validates :photo, :presence => true
+    # validates_attachment_size :photo, :in => 0.megabytes..0.5.megabytes
+    
+    has_and_belongs_to_many :gamers
 
-  attr_accessible :name, :level, :score, :photo
+    attr_accessible :name, :level, :score, :photo
 
-  # has_attached_file :photo
-
-  def self.get_new_prizes_for_gamer(gamer_id, score, level)
-    prizes_for_score = []
-    prizes_gamer = Gamer.find(gamer_id).prizes
-    prizes_of_level = Prize.where(:level => level)
-    new_prizes = prizes_of_level - prizes_gamer
-    new_prizes.map { |nt| prizes_for_score << nt if nt.score <= score }
-    return prizes_for_score
-  end
+    # has_attached_file :photo
+    
+    # Description:
+    #   returns a list of prizes that the gamer got awarded 
+    #   and awards it to the current_gamer
+    # Author:
+    #   Adam Ghanem
+    # @params:
+    #   gamer_id: the gamer id of the current signed in gamer
+    #   score: the score that was just scored in the game
+    #   level: the level that the gamer was on before the method was
+    #          called
+    # returns:
+    #   success:
+    #     an array of the prizes that the gamer won
+    #   failure:
+    #     no failure
+    def self.get_new_prizes_for_gamer(gamer_id, score, level)
+      prizes_for_score = []
+      prizes_gamer = Gamer.find(gamer_id).prizes
+      prizes_of_level = Prize.where(:level => level)
+      new_prizes = prizes_of_level - prizes_gamer
+      new_prizes.map { |nt| prizes_for_score << nt if nt.score <= score }
+      return prizes_for_score
+    end
 
 end
