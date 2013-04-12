4a561d85921b0fbdeb2ea81246dc4c9d053082cf
Salma Farag Galal <salmafarag94@gmail.com>
2013-04-12 21:54:11 +0200

    [WIP] Salma is creating tests

--- arability/spec/models/project_salma_spec.rb
+++ arability/spec/models/project_salma_spec.rb
@@ -23,6 +23,16 @@ describe "Create_Project" do
         developer.save
         developer
     }
+    let (:project) {
+        project = Project.new
+        project.name = "Project1"
+        project.minAge = "19"
+        project.maxAge = "50"
+        project.description = "This is a project"
+        project.formal = "formal"
+        project.save
+        project
+    }
     it "should return a new project after setting owner_id and calling createcategories" do
     	result = Project.createproject({:name => "Project1", :minAge => "20", :maxAge => "40",
             :formal => "formal", :description => "da da da", :categories => "Cat1, Cat2, Cat3"}, developer.id)
@@ -31,9 +41,7 @@ describe "Create_Project" do
     end
 
     it "should return a new project after creating categories and setting owner_id" do
-        result = Project.createcategories({:name => "Project1", :minAge => "20", :maxAge => "40",
-            :formal => "formal", :description => "da da da", :categories => ["Cat1", "Cat2", "Cat3"},
-             "Cat1, Cat2, Cat3")
+        result = Project.createcategories(project,"Cat1, Cat2, Cat3")
         result.should be_instance_of(Project)
     end
 end
