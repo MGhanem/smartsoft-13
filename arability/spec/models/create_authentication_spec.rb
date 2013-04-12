1047f1e48c341300eec6f5fc296deb53584e7fb6
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-12 12:58:12 +0200

    [#181][WIP] changed name of test for remove_conn

---
+++ arability/spec/models/create_authentication_spec.rb
@@ -0,0 +1,25 @@
+#encoding: utf-8
+require 'spec_helper'
+
+describe "create authentication to Twitter for current_gamer" do
+  let(:current_gamer){
+    current_gamer = Gamer.new
+    current_gamer.username = "Test"
+    current_gamer.country = "Egypt"
+    current_gamer.education_level = "high"
+    current_gamer.gender = "female"
+    current_gamer.date_of_birth = 1993-03-23
+    current_gamer.email = "test@gmail.com"
+    current_gamer.password = "1234567"
+    current_gamer.save
+    current_gamer
+  }
+  it "should create a new record for Twitter authentication in database for current_gamer" do
+    auth = Hash[provider: "twitter", uid: "123", credentials: [token: "1234567"], credentials: [secret: "asdfghj"]]
+    k1 = Authentication.exists?(gamer_id: current_gamer.id)
+    expect(k1).to eq(false)
+    record = Authentication.create_with_omniauth(auth, current_gamer)
+    k2 = Authentication.exists?(record.id)
+    expect(k1).to eq(true)
+  end
+end
