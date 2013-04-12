26e59c919dc00709f99de7cd23c06b43bc1eaf7b
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-13 01:42:39 +0200

    home page

--- arability/app/assets/javascripts/pages.js
+++ arability/app/assets/javascripts/pages.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var slide_show = $("div#slide_show");
-	$("div#main").prepend('<div id="slide_show" class="carousel slide">' + slide_show.html() + '</div>');
-	slide_show.remove();
-	$('.carousel').carousel();
+	//var slide_show = $("div#slide_show");
+	//$("div#main").prepend('<div id="slide_show" class="carousel slide">' + slide_show.html() + '</div>');
+	//slide_show.remove();
+	//$('.carousel').carousel();
 });
