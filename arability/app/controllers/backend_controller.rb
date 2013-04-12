c8d20c759cb79969da6c5de19865fbf8c6ffcb72
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-04-12 19:46:06 +0200

    [Issue #210][WIP] added documentation for developer authentication

--- arability/app/controllers/backend_controller.rb
+++ arability/app/controllers/backend_controller.rb
@@ -3,7 +3,17 @@ class BackendController < ApplicationController
   def home
     redirect_to projects_path
   end
-  
+
+  # Author:
+  #   Mohamed Tamer
+  # Description
+  #   checks if developer is signed in or not
+  # Params:
+  #   current_gamer: the current signed in gamer
+  # Success: 
+  #   continues as normal 
+  # Failure:
+  #   redirects to creating new developer
   def authenticate_developer!
     developer = Developer.where(:gamer_id => current_gamer.id).first
   	if developer == nil
@@ -12,6 +22,16 @@ class BackendController < ApplicationController
   	end
   end
 
+  # Author:
+  #   Mohamed Tamer
+  # Description
+  #   checks if the project the developer is trying to access is in his owned projects or shared
+  # Params:
+  #   current_gamer: the current signed in gamer
+  # Success: 
+  #   continues as normal
+  # Failure:
+  #   redirects back to projects path
   def developer_can_see_this_project?
     developer = Developer.where(:gamer_id => current_gamer.id).first
     projects_owned = Project.where(:owner_id => developer.id)
