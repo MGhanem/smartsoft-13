8a934620eaed5a2c7b6d8fd50b4105c656670278
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-04-12 11:08:56 +0200

    [Issue #94] Added new lines in tests

--- arability/spec/models/keyword_spec.rb
+++ arability/spec/models/keyword_spec.rb
@@ -7,16 +7,19 @@ describe Keyword do
     success, keyword = Keyword.add_keyword_to_database("ينسب")
     success.should eq(true)
   end
+
   it "Doesnt add keyword to database if english and arabic letters" do
     success, keyword = Keyword.add_keyword_to_database("ينسب dj")
     success.should eq(false)
   end
+
   it "Doesnt add keyword to database if anything other than letters" do
     success, keyword = Keyword.add_keyword_to_database("test2")
     success.should eq(false)
     success, keyword = Keyword.add_keyword_to_database("ستمب2")
     success.should eq(false)
   end
+
   it "should strip keyword before adding to database" do
     success, keyword = Keyword.add_keyword_to_database("test ")
     success.should eq(true)
@@ -25,17 +28,20 @@ describe Keyword do
     success.should eq(true)
     keyword.name.should eq("ينسب")
   end
+
   it "should downcase keyword before adding to database" do
     success, keyword = Keyword.add_keyword_to_database("Dj")
     success.should eq(true)
     keyword.name.should eq("dj")
   end
+
   it "should find keyword by name" do
     success, keyword = Keyword.add_keyword_to_database("dj")
     success.should eq(true)
     keyword2 = Keyword.find_by_name("dj")
     keyword2.id.should eq(keyword.id)
   end
+
 	it "should return an empty list for an empty search keyword" do
 		Keyword.create(name: "click", approved: true)
 		Keyword.create(name: "clickMe", approved: true)
@@ -89,7 +95,7 @@ describe Keyword do
 		keyword.should eq([])
 	end
 
-		it "should return one matching keyword for the passed param in arabic" do
+	it "should return one matching keyword for the passed param in arabic" do
 		Keyword.create(name: "click", approved: true)
 		Keyword.create(name: "clickMe", approved: true)
 		Keyword.create(name: "click me")
