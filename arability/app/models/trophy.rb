f8d73122c76dc5d06a28a4f58033e480188ef031
Salma Farag Galal <salmafarag94@gmail.com>
2013-04-15 19:50:46 +0200

    [FIX MERGE CONFLICT] fix add prize validation and form and delete old files resulted from a conflict

--- arability/app/models/trophy.rb
+++ arability/app/models/trophy.rb
@@ -50,53 +50,53 @@ class Trophy < ActiveRecord::Base
                             :in => 0.megabytes..1.megabytes,
                             message: "يجب ان يكون حجم الصورة اقل من 1MB"
 
-    # Description:
-    #   returns a list of trophies that the gamer got awarded 
-    #   and awards it to the current_gamer
-    # Author:
-    #   Adam Ghanem
-    # @params:
-    #   gamer_id: the gamer id of the current signed in gamer
-    #   score: the score that was just scored in the game
-    #   level: the level that the gamer was on before the method was
-    #          called
-    # returns:
-    #   success:
-    #     an array of the trophies that the gamer won
-    #   failure:
-    #     no failure
-    def self.get_new_trophies_for_gamer(gamer_id, score, level)
-      trophies_for_score = []
-      gamer = Gamer.find(gamer_id)
-      trophies_gamer = gamer.trophies
-      trophies_of_level = Trophy.where(:level => level)
-      new_trophies = trophies_of_level - trophies_gamer
-      new_trophies.map { |nt| trophies_for_score << nt if nt.score <= score }
-      trophies_for_score.map { |t| gamer.trophies << t }
-      gamer.save
-      return trophies_for_score
-    end
+  # Description:
+  #   returns a list of trophies that the gamer got awarded 
+  #   and awards it to the current_gamer
+  # Author:
+  #   Adam Ghanem
+  # @params:
+  #   gamer_id: the gamer id of the current signed in gamer
+  #   score: the score that was just scored in the game
+  #   level: the level that the gamer was on before the method was
+  #          called
+  # returns:
+  #   success:
+  #     an array of the trophies that the gamer won
+  #   failure:
+  #     no failure
+  def self.get_new_trophies_for_gamer(gamer_id, score, level)
+    trophies_for_score = []
+    gamer = Gamer.find(gamer_id)
+    trophies_gamer = gamer.trophies
+    trophies_of_level = Trophy.where(:level => level)
+    new_trophies = trophies_of_level - trophies_gamer
+    new_trophies.map { |nt| trophies_for_score << nt if nt.score <= score }
+    trophies_for_score.map { |t| gamer.trophies << t }
+    gamer.save
+    return trophies_for_score
+  end
 
-    # author:
-    #     Karim ElNaggar
-    # description:
-    #     a function adds a new trophy to the database
-    # params
-    #     name: the name of the trophy
-    #     level: the level of the trophy
-    #     score: the score of the trophy 
-    #     image: the image of the trophy6
-    # success: 
-    #     returns true and the new trophy if it is added to the database
-    # failure: 
-    #     returns false and the trophy if it is not added to the database
-    def self.add_trophy_to_database(name, level, score, image)
-      new_trophy = Trophy.new(name: name, level: level, score: score, image: image)
-      if new_trophy.save
-        return true, new_trophy
-      else
-        return false, new_trophy
-      end
+  # author:
+  #     Karim ElNaggar
+  # description:
+  #     a function adds a new trophy to the database
+  # params
+  #     name: the name of the trophy
+  #     level: the level of the trophy
+  #     score: the score of the trophy 
+  #     image: the image of the trophy6
+  # success: 
+  #     returns true and the new trophy if it is added to the database
+  # failure: 
+  #     returns false and the trophy if it is not added to the database
+  def self.add_trophy_to_database(name, level, score, image)
+    new_trophy = Trophy.new(name: name, level: level, score: score, image: image)
+    if new_trophy.save
+      return true, new_trophy
+    else
+      return false, new_trophy
     end
+  end
 
 end
