6d1c4df69aa32dcfe5c1be910d91f07c7c642039
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-17 20:27:03 +0200

    [#81][WIP] updated documentation from approval to disapproval

--- arability/app/models/synonym.rb
+++ arability/app/models/synonym.rb
@@ -56,15 +56,15 @@ class Synonym < ActiveRecord::Base
   # Author:
   #  Mirna Yacout
   # Description:
-  #  This method is to record the aproval of the admin to a certain synonym 
+  #  This method is to record the disapproval of the admin to a certain synonym 
   #   in the database
   # Parameters:
-  #  id: the id of the synonym to be approved
+  #  id: the id of the synonym to be disapproved
   # Success:
-  #  returns true on saving the approval correctly in the database
+  #  returns true on saving the disapproval correctly in the database
   # Failure:
   #  returns false if the synonym doesnot exist in the database
-  #  or if the approval failed to be saved in the database     
+  #  or if the disapproval failed to be saved in the database     
 	def disapprove_synonym(synonym_id)
     if Synonym.exists?(id: synonym_id)
       synonym = Synonym.find(synonym_id)
