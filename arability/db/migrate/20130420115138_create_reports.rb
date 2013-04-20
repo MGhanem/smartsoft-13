2280d11c4fe5f8e977ca9080401110256ae57466
Kareem <lkareem.alil@gmail.com>
2013-04-21 01:56:50 +0200

    fixes the migration

--- arability/db/migrate/20130420115138_create_reports.rb
+++ arability/db/migrate/20130420115138_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
     	t.references :reported_word, :polymorphic => true
-    	t.references :gamers
+    	t.references :gamer
     	t.timestamps
     end
   end
