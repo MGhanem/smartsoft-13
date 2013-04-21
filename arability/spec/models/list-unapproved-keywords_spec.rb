93694bae2ba67b6dee2c183b9a0051b75b1844cf
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-21 17:24:37 +0200

    [WIP][issue #89] tests modified

--- arability/spec/models/list-unapproved-keywords_spec.rb
+++ arability/spec/models/list-unapproved-keywords_spec.rb
@@ -1,7 +1,7 @@
 #encoding: UTF-8
 require "spec_helper"
 
-describe "ListUnapprovedKeywordsTest_Omar" do
+describe "ListUnapproved/Approved/ReportedKeywordsTest_Omar" do
 
     let(:word) {word = Keyword.new
     word.name = "testkeyword"
@@ -55,7 +55,7 @@ describe "ListUnapprovedKeywordsTest_Omar" do
     expect(result2).to eq ("testkeywordfour")
   end
 
-  it "should return an empty array because no synonyms are approved" do
+  it "should return an empty array because no synonyms are unapproved" do
     word.approved = true
     word.save
     word2.approved = true
@@ -65,6 +65,88 @@ describe "ListUnapprovedKeywordsTest_Omar" do
     word4.approved = true
     word4.save
     result = Keyword.list_unapproved_keywords
+    expect(result).to eq ([])
+  end
+
+  it "should return the approved keyword only" do
+    word.approved = false
+    word.save
+    word2.approved = false
+    word2.save
+    word3.approved = false
+    word3.save
+    word4.approved = true
+    word4.save
+    result = Keyword.list_approved_keywords
+    expect(result.first.name).to eq ("testkeywordfour")
+  end
+
+  it "should return an array of Keywords with approved synonyms" do
+    word.approved = true
+    word.save
+    word2.approved = false
+    word2.save
+    word3.approved = true
+    word3.save
+    word4.approved = false
+    word4.save
+    result1 = Keyword.list_approved_keywords.first.name
+    result2 = Keyword.list_approved_keywords.second.name
+    expect(result1).to eq ("testkeyword")
+    expect(result2).to eq ("testkeywordthree")
+  end
+
+  it "should return an empty array because no synonyms are approved" do
+    word.approved = false
+    word.save
+    word2.approved = false
+    word2.save
+    word3.approved = false
+    word3.save
+    word4.approved = false
+    word4.save
+    result = Keyword.list_approved_keywords
+    expect(result).to eq ([])
+  end
+
+  it "should return the reported keyword only" do
+    word.reported = false
+    word.save
+    word2.reported = false
+    word2.save
+    word3.reported = false
+    word3.save
+    word4.reported = true
+    word4.save
+    result = Keyword.list_reported_keywords
+    expect(result.first.name).to eq ("testkeywordfour")
+  end
+
+  it "should return an array of reported Keywords" do
+    word.reported = true
+    word.save
+    word2.reported = false
+    word2.save
+    word3.reported = true
+    word3.save
+    word4.reported = false
+    word4.save
+    result1 = Keyword.list_reported_keywords.first.name
+    result2 = Keyword.list_reported_keywords.second.name
+    expect(result1).to eq ("testkeyword")
+    expect(result2).to eq ("testkeywordthree")
+  end
+
+  it "should return an empty array because no arrays are reported" do
+    word.reported = false
+    word.save
+    word2.reported = false
+    word2.save
+    word3.reported = false
+    word3.save
+    word4.reported = false
+    word4.save
+    result = Keyword.list_reported_keywords
     expect(result).to eq ([])
   end
 end
