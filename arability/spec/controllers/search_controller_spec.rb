30626028bc79ac7a8c0bdba870281dbcb797467f
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-21 16:26:50 +0200

    [Issue #364][WIP] added controller tests for autocomplete controller action

--- arability/spec/controllers/search_controller_spec.rb
+++ arability/spec/controllers/search_controller_spec.rb
@@ -36,7 +36,7 @@ describe SearchController do
       gamer.date_of_birth = "1993-03-23"
       gamer.email = "nour@gmail.com"
       gamer.password = "1234567"
-      gamer.saveg = Gamer.new
+      gamer.save(validate: false)
     end
 
     it "should get only keywords in category" do
@@ -63,7 +63,42 @@ describe SearchController do
       k
       k2
       get :search_keywords, search: "test"
-      assigns :search_keywords.should == [k, k2]
+      assigns(:similar_keywords).should == [k, k2]
+      assigns(:similar_keywords).should_not == [k2, k]
+    end
+
+    it "should return json containing similar keywords" do
+      d = create_logged_in_developer()
+      sign_in(d.gamer)
+      k
+      k2
+      k3
+      get :keyword_autocomplete, search: "test"
+      json_response = JSON(response.body)
+      json_response.first.should eq("test")
+      json_response.last.should eq("testing")
+    end
+
+    it "should return empty json on not-found" do
+      d = create_logged_in_developer()
+      sign_in(d.gamer)
+      k
+      k2
+      k3
+      get :keyword_autocomplete, search: "click"
+      json_response = JSON(response.body)
+      json_response.should == []
+    end
+
+    it "should return empty json on non-approved keywords" do
+      d = create_logged_in_developer()
+      sign_in(d.gamer)
+      k
+      k2
+      k3
+      get :keyword_autocomplete, search: "ktest"
+      json_response = JSON(response.body)
+      json_response.should == []
     end
   end
 end
