9350e77db82dab4a2c8870f6c71fd679184b90d6
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-04-22 18:59:36 +0200

    [Issue #92] fixed code conventions

--- arability/spec/models/keyword_spec.rb
+++ arability/spec/models/keyword_spec.rb
@@ -49,7 +49,7 @@ describe Keyword do
 
   it "should add keywords with categories" do
     success, category = Category.add_category_to_database_if_not_exists("test", "تشتبت")
-    success, keyword = Keyword.add_keyword_to_database("test",true,true, ["تشتبت"])
+    success, keyword = Keyword.add_keyword_to_database("test", true, true, ["تشتبت"])
     success.should eq(true)
     category.keywords.include?(keyword).should eq(true)
     success, keyword = Keyword.add_keyword_to_database("ينسب", true, false, ["تشتبت"])
