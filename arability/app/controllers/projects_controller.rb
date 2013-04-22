3c9b7711abcc6e9b33e01645af44a8ee2586363c
Noha Hesham <nohahesham16@gmail.com>
2013-04-22 13:53:19 +0200

    [WIP]

--- arability/app/controllers/projects_controller.rb
+++ arability/app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < BackendController
   # GET /projects.json
   before_filter :authenticate_gamer!
   before_filter :authenticate_developer!
-  before_filter :developer_can_see_this_project?, :only => [:import_csv, :show, :add_from_csv_keywords, :choose_keywords]
+  before_filter :developer_can_see_this_project?, :only => [:import_csv, :show, :add_from_csv_keywords, :choose_keywords, :destroy]
 
 
 
