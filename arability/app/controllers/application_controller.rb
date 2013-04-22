ca1f9a48b905e8808238e5111f686b7dbb80b11a
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-04-22 11:30:05 +0300

    [Issue #193] Fixed code convention again

--- arability/app/controllers/application_controller.rb
+++ arability/app/controllers/application_controller.rb
@@ -34,6 +34,6 @@ class ApplicationController < ActionController::Base
   # failure:
   #   --
   def default_url_options(options={})
-    { :locale => I18n.locale }
+    { locale: I18n.locale }
   end
 end
