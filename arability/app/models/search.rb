7b2309e3a7a316bf2947fccdb9887338c7a1e2f9
Noha Hesham <nohahesham16@gmail.com>
2013-04-22 12:28:02 +0200

    [WIP][issue #206] Managing subscription model from account settings

--- arability/app/models/search.rb
+++ arability/app/models/search.rb
@@ -3,4 +3,15 @@ class Search < ActiveRecord::Base
   belongs_to :keyword
   belongs_to :developer
 
+  def get_search_permission(word_id,dev_id)
+  	my_subscription = 
+         MySubscription.joins(:developer).where(:developer_id => dev_id).first
+    word = Search.joins(:keyword).where(:keyword_id => word_id)
+    developer = Search.joins(:developer).where(:developer_id => dev_id)
+    if Search.find(keyword_id => word_id)!= nil
+    	return true
+    else 
+    	
+  end
+
 end
