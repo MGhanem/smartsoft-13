3b40d3ac8913960cfc79fb331fbff41886cdcebe
Khloud Khalid <khloud.khalid7@gmail.com>
2013-04-23 16:39:29 +0200

    [Issue #85] [WIP] fixes controller tests

--- arability/spec/controllers/developer_controller_khloud_spec.rb
+++ arability/spec/controllers/developer_controller_khloud_spec.rb
@@ -6,31 +6,16 @@ include Warden::Test::Helpers
 
 describe DeveloperController do
 
-    let(:gamer) {
-      gamer = Gamer.new
-      gamer.username = "Khloud"
-      gamer.country = "Egypt"
-      gamer.education_level = "high"
-      gamer.gender = "female"
-      gamer.date_of_birth = "1993-08-02"
-      gamer.email = "khloud.khalid7@gmail.com"
-      gamer.password = "1234567"
-      gamer.save
-      gamer
-    }
-
     it "should redirect to project path if current gamer is already a developer" do
-      d = Developer.new
-      d.verified = true
-      d.gamer_id = gamer.id 
-      d.save
-      current_gamer = gamer
+      d = create_logged_in_developer()
+      sign_in(d.gamer)
       get :new
       response.should redirect_to :projects_path
     end
 
     it "should create new developer and redirect to choose subscription model page if current gamer is not a developer" do
-      current_gamer = gamer
+      g = create_logged_in_user()
+      sign_in(g)
       get :new
       assigns(@developer.gamer_id).should == gamer.id
       response.should redirect_to :choose_sub_path
