69bd5a8ef163f9da0dddc5ee2f28e74c011abb72
Khloud Khalid <khloud.khalid7@gmail.com>
2013-04-23 16:58:54 +0200

    [Issue #85] [WIP] fixes controller tests

--- arability/spec/controllers/developer_controller_khloud_spec.rb
+++ arability/spec/controllers/developer_controller_khloud_spec.rb
@@ -8,14 +8,14 @@ describe DeveloperController do
 
     it "should redirect to project path if current gamer is already a developer" do
       d = create_logged_in_developer()
-      sign_in(d.gamer)
+      login(d.gamer)
       get :new
       response.should redirect_to :projects_path
     end
 
     it "should create new developer and redirect to choose subscription model page if current gamer is not a developer" do
       g = create_logged_in_user()
-      sign_in(g)
+      login(g)
       get :new
       assigns(@developer.gamer_id).should == gamer.id
       response.should redirect_to :choose_sub_path
