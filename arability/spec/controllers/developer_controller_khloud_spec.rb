2487b6a72beff3518ed89bcf1fa2c3b6984cdf77
Khloud Khalid <khloud.khalid7@gmail.com>
2013-04-23 19:55:21 +0200

    [Issue #85] [WIP] fixes controller tests

--- arability/spec/controllers/developer_controller_khloud_spec.rb
+++ arability/spec/controllers/developer_controller_khloud_spec.rb
@@ -16,6 +16,12 @@ describe DeveloperController, :type => :controller do
     it "should create new developer and redirect to choose subscription model page if current gamer is not a developer" do
       g = create_logged_in_user()
       sign_in(g)
+      sm = SubscriptionModel.new
+      sm.name = "Free Trial"
+      sm.limit_search = 20
+      sm.limit_follow = 20
+      sm.limit_project = 20
+      sm.save
       get :new
       response.should redirect_to choose_sub_path
     end
