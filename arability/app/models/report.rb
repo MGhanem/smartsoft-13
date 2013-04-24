e23c3d12c61746ea18b83028a3d3d92c04fdb5bc
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-24 15:06:55 +0200

    [Issue #366][WIP] updated report model validations and methods

--- arability/app/models/report.rb
+++ arability/app/models/report.rb
@@ -2,4 +2,16 @@ class Report < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :reported_word, :polymorphic => true 
   belongs_to :gamer
+  validates_presence_of :gamer
+  validates_presence_of :reported_word
+  validates :gamer_id, :uniqueness => { :scope => :reported_word_id,
+    :message => "You reported this word before" }
+
+  def self.new_report(gamer, word)
+    report = Report.new
+    report.reported_word = word
+    report.gamer = gamer
+    success = report.save
+    [success, report]
+  end
 end
