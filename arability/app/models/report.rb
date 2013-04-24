a65490aa37beaab9ee789e091275f928463e2ea0
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-24 15:55:27 +0200

    [Issue #366][WIP] applied code conventions on report model

--- arability/app/models/report.rb
+++ arability/app/models/report.rb
@@ -1,11 +1,11 @@
 class Report < ActiveRecord::Base
   # attr_accessible :title, :body
-  belongs_to :reported_word, :polymorphic => true 
+  belongs_to :reported_word, polymorphic: true 
   belongs_to :gamer
   validates_presence_of :gamer
   validates_presence_of :reported_word
-  validates :gamer_id, :uniqueness => { :scope => :reported_word_id,
-    :message => "You reported this word before" }
+  validates :gamer_id, uniqueness: { scope: :reported_word_id,
+    message: "You reported this word before" }
 
   # Author:
   #   Nourhan Mohamed
