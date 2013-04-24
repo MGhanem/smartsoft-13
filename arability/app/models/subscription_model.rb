38c56a92239ec0ca3e98c53cfab578edcb60c9f9
Noha Hesham <nohahesham16@gmail.com>
2013-04-24 14:13:51 +0200

    [WIP] removing first name and last name

--- arability/app/models/subscription_model.rb
+++ arability/app/models/subscription_model.rb
@@ -1,4 +1,4 @@
 class SubscriptionModel < ActiveRecord::Base
 	has_many :my_subscriptions
-  attr_accessible :name, :limit_search, :limit_follow, :limit_project
+  attr_accessible :name, :limit_search, :limit_follow, :limit_project ,:limit
 end
