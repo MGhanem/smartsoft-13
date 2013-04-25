fc05fc07c77da0078dbcb92deeb9152da7c2c348
Adam G <ghanem.adam@gmail.com>
2013-04-25 16:04:03 +0200

    [Issue #171][WIP] restyled css and finished view

--- arability/config/routes.rb
+++ arability/config/routes.rb
@@ -37,6 +37,7 @@ Arability::Application.routes.draw do
     get 'games/gettrophies'
     get 'games/getnewwords'
     get "games/showprizes"
+    get "games/showtrophies"
     get "games/get_score_only"
     post "games/record_synonym"
     get "/games/halloffame"
