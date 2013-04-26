a3b767ae4ba741b2425c5aac52a494daf4d56b16
Noha Hesham <nohahesham16@gmail.com>
2013-04-26 16:34:29 +0200

    [issue #206]

--- arability/app/controllers/my_subscription_controller.rb
+++ arability/app/controllers/my_subscription_controller.rb
@@ -99,7 +99,7 @@ class MySubscriptionController < ApplicationController
     sub_id = params[:my_subscription]
     dev_id = @developer.id
     if MySubscription.choose(dev_id, sub_id)
-      flash[:success] = I18n.t('controller.subscription.messages_errors.you_have_successfully_chosen_your_model')
+      flash[:success] = I18n.t('controller.subscription.messages_errors.you_have_successfully_chosen_your_model') 
       redirect_to projects_path
     else
       flash[:notice] = I18n.t('controller.subscription.messages_errors.please_choose')
