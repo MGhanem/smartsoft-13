fc993f60686d706531a239ad733ffcb4bcc944eb
Salma <salma@Brownie.(none)>
2013-04-26 21:59:01 +0200

    [Issue #77][WIP] Added 2 columns to projects table

--- arability/db/migrate/20130423085416_change_projects.rb
+++ arability/db/migrate/20130423085416_change_projects.rb
@@ -1,6 +1,8 @@
 class ChangeProjects < ActiveRecord::Migration
   def up
     add_column :projects, :category_id, :integer
+    add_column :projects, :country, :string
+    add_column :projects, :education_level, :string
   end
 
   def down
