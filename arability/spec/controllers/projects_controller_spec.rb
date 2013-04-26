43b2e0edb4f361b7f48f239e17118712bb3d575e
Kareem <lkareem.alil@gmail.com>
2013-04-26 23:41:09 +0200

    [issue #353] [Fixed] fixed test files

--- arability/spec/controllers/projects_controller_spec.rb
+++ arability/spec/controllers/projects_controller_spec.rb
@@ -105,7 +105,16 @@ describe ProjectsController, type: :controller do
 
   let(:test_keyword){
     test_keyword = Keyword.new
-    test_keyword.name = "airplane"
+    test_keyword.name = "desk"
+    test_keyword.approved = true
+    test_keyword.is_english = true
+    test_keyword.save
+    test_keyword
+  }
+
+  let(:test_keyword2){
+    test_keyword = Keyword.new
+    test_keyword.name = "download"
     test_keyword.approved = true
     test_keyword.is_english = true
     test_keyword.save
@@ -145,32 +154,72 @@ describe ProjectsController, type: :controller do
   let(:prefered_synonym){
     prefered_synonym = PreferedSynonym.new
     prefered_synonym.project_id = test_project.id
-    prefered_synonym.keyword_id = word.id
-    prefered_synonym.synonym_id = syn.id
+    prefered_synonym.keyword_id = test_keyword.id
+    prefered_synonym.synonym_id = synonym1.id
     prefered_synonym.save
     prefered_synonym
   }
 
-  it "should add a keyword and a prefered synonym in a project and redirects to project view", kareem:true do
+  let(:category){
+    category = Category.new
+    category.english_name = "bank"
+    category.arabic_name =  "بنك"
+    category.save
+    category
+  }
+
+  before(:each) do
     developer = create_logged_in_developer
     sign_in(developer.gamer)
-    post :quick_add, project_id: project.id, keyword: word.name, synonym_id: syn.id
+    test_keyword
+    synonym1
+    synonym2
+    test_keyword2
+    test_project
+    prefered_synonym
+    category
+  end
+
+  it "should add a keyword and a prefered synonym in a project and redirects to project view", kareem: true do
+    post :add_word_inside_project, project_id: project.id, keyword: word.name, synonym_id: syn.id
     prefered_synonyms = PreferedSynonym.where(project_id: project.id)
     saved_prefered_synonym = prefered_synonyms.where(keyword_id: word.id).first
     saved_prefered_synonym.synonym_id.should eq(syn.id)
     response.should redirect_to project_path(project.id)
   end
 
-  it "should add new synonym to an existing keyword if keyword added again and redirects to project view", kareem:true do
-    developer = create_logged_in_developer
-    sign_in(developer.gamer) 
-    post :quick_add, project_id: test_project.id, keyword: word.name, synonym_id: synonym1.id
+  it "should redirects to project view when new synonym to an existing keyword",
+   kareem:true do
+    post :quick_add, project_id: test_project.id, keyword: test_keyword.name, synonym_id: synonym2.id
     prefered_synonyms = PreferedSynonym.where(project_id: test_project.id)
-    saved_prefered_synonym = prefered_synonyms.where(keyword_id: word.id).first
-    prefered_synonyms.count.should eq(1)
-    saved_prefered_synonym.synonym_id.should eq(synonym1.id)
+    saved_prefered_synonym = prefered_synonyms.where(keyword_id: test_keyword.id).first
     response.should redirect_to project_path(test_project.id)
   end
 
+  it "should change synonym to an existing keyword and redirects to project view",
+   kareem:true do
+    post :add_word_inside_project, project_id: test_project.id, 
+    keyword: test_keyword.name, synonym_id: synonym2.id
+    prefered_synonyms = PreferedSynonym.where(project_id: test_project.id)
+    saved_prefered_synonym = prefered_synonyms.where(keyword_id: test_keyword.id).first
+    saved_prefered_synonym.synonym_id.should eq(synonym2.id)
+    prefered_synonyms.count.should eq(1)
+    response.should redirect_to project_path(test_project.id)
+  end
+
+  it "should render load_synonym.js file and succeed", kareem: true do
+    post :load_synonyms, project_id: test_project.id, word: test_keyword.name
+    response.should render_template("projects/load_synonyms.js")
+    response.should be_success
+  end
+
+  it "shoud succed on sending a keyword for autocomplete", kareem: true do
+    post :project_keyword_autocomplete, keyword_search:"d", project_id: test_project.id
+    test_project.categories << category
+    test_keyword.categories << category
+    similar_keyword = [test_keyword.name, test_keyword2.name, 2]
+    response.body.should == similar_keyword.to_json
+    response.should be_success
+  end
 
 end
