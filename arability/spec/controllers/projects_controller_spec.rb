3dc40491dcb20dce9b1a6bdf01dbe7801387ba3b
Kareem <lkareem.alil@gmail.com>
2013-04-26 20:41:46 +0200

    [issue #353] [Fixed] fixed tests

--- arability/spec/controllers/projects_controller_spec.rb
+++ arability/spec/controllers/projects_controller_spec.rb
@@ -101,4 +101,76 @@ describe ProjectsController, type: :controller do
     response.code.should eq("200")
   end
 
+  #Kareem Ali tests
+
+  let(:test_keyword){
+    test_keyword = Keyword.new
+    test_keyword.name = "airplane"
+    test_keyword.approved = true
+    test_keyword.is_english = true
+    test_keyword.save
+    test_keyword
+  }
+
+  let(:synonym1){
+    synonym1 = Synonym.new
+    synonym1.name = "ايربلان"
+    synonym1.approved = true
+    synonym1.keyword_id = test_keyword.id
+    synonym1.save
+    synonym1
+  }
+
+  let(:synonym2){
+    synonym2 = Synonym.new
+    synonym2.name = "طيارة"
+    synonym2.approved = true
+    synonym2.keyword_id = test_keyword.id
+    synonym2.save
+    synonym2
+  }
+
+  let(:test_project){
+    project = Project.new
+    project.name = "test"
+    project.formal = true
+    project.minAge = 10
+    project.maxAge = 90
+    project.owner_id = developer1.id
+    project.description = "this is a test project" 
+    project.save
+    project
+  }
+
+  let(:prefered_synonym){
+    prefered_synonym = PreferedSynonym.new
+    prefered_synonym.project_id = test_project.id
+    prefered_synonym.keyword_id = word.id
+    prefered_synonym.synonym_id = syn.id
+    prefered_synonym.save
+    prefered_synonym
+  }
+
+  it "should add a keyword and a prefered synonym in a project and redirects to project view", kareem:true do
+    developer = create_logged_in_developer
+    sign_in(developer.gamer)
+    post :quick_add, project_id: project.id, keyword: word.name, synonym_id: syn.id
+    prefered_synonyms = PreferedSynonym.where(project_id: project.id)
+    saved_prefered_synonym = prefered_synonyms.where(keyword_id: word.id).first
+    saved_prefered_synonym.synonym_id.should eq(syn.id)
+    response.should redirect_to project_path(project.id)
+  end
+
+  it "should add new synonym to an existing keyword if keyword added again and redirects to project view", kareem:true do
+    developer = create_logged_in_developer
+    sign_in(developer.gamer) 
+    post :quick_add, project_id: test_project.id, keyword: word.name, synonym_id: synonym1.id
+    prefered_synonyms = PreferedSynonym.where(project_id: test_project.id)
+    saved_prefered_synonym = prefered_synonyms.where(keyword_id: word.id).first
+    prefered_synonyms.count.should eq(1)
+    saved_prefered_synonym.synonym_id.should eq(synonym1.id)
+    response.should redirect_to project_path(test_project.id)
+  end
+
+
 end
