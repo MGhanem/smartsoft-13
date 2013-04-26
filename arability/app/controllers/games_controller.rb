af5d257f3c31441d22c4c9f495058634e944333c
Adam G <ghanem.adam@gmail.com>
2013-04-26 19:57:37 +0200

    [Issue #165][WIP] Finished controller tests

--- arability/app/controllers/games_controller.rb
+++ arability/app/controllers/games_controller.rb
@@ -256,27 +256,7 @@ class GamesController < ApplicationController
       format.js
     end
   end
-  
-  # Description:
-  #   Returns the view that the gamer can view the trophies that 
-  #   are available currently in the game and the trophies that they
-  #   have already earned
-  # Author:
-  #   Adam Ghanem
-  # @params:
-  #   none
-  # returns:
-  #   success: renders out a view using js erb view with the 
-  #   earned trophies in a list and the trophies that haven't been 
-  #   earned in another
-  def showtrophies
-    @won_trophies = current_gamer.get_won_trophies
-    @not_won_trophies = current_gamer.get_available_trophies
-    respond_to do |format|
-      format.js
-    end
-  end
-  
+ 
   # Author:
   # 	Kareem Ali
   # Description:
