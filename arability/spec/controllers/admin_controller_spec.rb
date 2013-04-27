22738f1411722b1da502ce2c365fd5d19f9bb8c8
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-27 23:03:02 +0200

    [WIP][issue #369] tests in progress

--- arability/spec/controllers/admin_controller_spec.rb
+++ arability/spec/controllers/admin_controller_spec.rb
@@ -1,42 +1,52 @@
 #encoding: UTF-8
 require "spec_helper"
+require "request_helpers"
+include RequestHelpers
+include Warden::Test::Helpers
 
 describe AdminController  do
-	
-	let(:model1){
-	  model = SubscriptionModel.new
+  describe "GET #subscription_model" do
+    include Devise::TestHelpers
+    let(:model1) do
+    model = SubscriptionModel.new
     model.name = "Test"
     model.limit_search = "300"
     model.limit_follow = "200"
     model.limit_project = "100"
     model.save
     model
-  }
-
-	it "should login the user if correct username and password" do
-		get :login
-		expect(response.code).to eq("200")
-	end
-
-	it "should redirect to login page" do
-		get :index
-		response.should redirect_to :action => :login
-	end
-
-	it "renders the login template" do
-      get :login
-      expect(response).to render_template("login")
-    end
-
+  end
     it "list all subscription models" do
+      model1
+      post :login, username: "admin", password: "admin"
       get :subscription_model
-      puts response.inspect
-      response.code.should eq("302")
+      assigns(:@models).should =~ [model1]
     end
+  end
+end	
+	
 
-    # it "list all subscription modelss" do
-    #   get :edit_subscription_model, model_id: model1.id
-    #   response.code.should eq("302")
-    # end
     
-end
+
+	# it "should login the user if correct username and password" do
+	# 	get :login
+	# 	expect(response.code).to eq("200")
+	# end
+
+	# it "should redirect to login page" do
+	# 	get :index
+	# 	response.should redirect_to :action => :login
+	# end
+
+	# it "renders the login template" do
+ #      get :login
+ #      expect(response).to render_template("login")
+ #    end
+
+    
+
+ #    # it "list all subscription modelss" do
+ #    #   get :edit_subscription_model, model_id: model1.id
+ #    #   response.code.should eq("302")
+ #    # end
+ #  end  
