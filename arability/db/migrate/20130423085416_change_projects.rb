6f99315f67901e1cf62f7daf3dc905b60785ca75
Salma <salma@Brownie.(none)>
2013-04-27 10:03:05 +0200

    [Issue #77][WIP] Fixed previous migration

--- arability/db/migrate/20130423085416_change_projects.rb
+++ arability/db/migrate/20130423085416_change_projects.rb
@@ -1,8 +1,6 @@
 class ChangeProjects < ActiveRecord::Migration
   def up
     add_column :projects, :category_id, :integer
-    add_column :projects, :country, :string
-    add_column :projects, :education_level, :string
   end
 
   def down
