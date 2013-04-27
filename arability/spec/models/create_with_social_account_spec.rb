c504751fd54866fa1cc402d1d583c72e66d41b73
Amr Hatem Abdelraouf <amr.abdelraouf92@gmail.com>
2013-04-27 14:55:11 +0200

    [#358] adds incomplete test file

---
+++ arability/spec/models/create_with_social_account_spec.rb
@@ -0,0 +1,11 @@
+#encoding: UTF-8
+require 'spec_helper'
+
+describe "Test create with social account function in gamer model" do
+  it "saves the new gamer in the database with the is_local field set to false and returns true" do
+    gamer, was_saved = Gamer.create_with_social_account(
+      "amr123456@fakemail.com", "AMR123456", "male", "1993-11-23", "Egypt", "School")
+    expect(was_saved).to eq(true)
+    # expect(gamer.is_local).to eq(false)
+  end
+end
