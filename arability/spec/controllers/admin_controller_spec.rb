822c73ce453481776c28f6f6bfde5d93b57280ca
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-28 01:00:13 +0200

    [WIP][issue #369] test modified

--- arability/spec/controllers/admin_controller_spec.rb
+++ arability/spec/controllers/admin_controller_spec.rb
@@ -15,6 +15,20 @@ describe AdminController  do
       model.save validate: false
       model
     end
+    it "should login the user if correct username and password" do
+      get :login
+      expect(response.code).to eq("200")
+    end
+
+    it "should redirect to login page" do
+      get :index
+      response.should redirect_to :action => :login
+    end
+
+    it "renders the login template" do
+      get :login
+      expect(response).to render_template("login")
+    end 
     it "list all subscription models" do
       model1
       post :login, username: "admin", password: "admin"
