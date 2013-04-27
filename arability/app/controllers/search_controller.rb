d2ae46c9408e7ec2042986c997a21fffa294c6fe
Khloud Khalid <khloud.khalid7@gmail.com>
2013-04-27 15:41:14 +0200

    [Issue #179] [WIP] adds functionality to 'add to project' button in search page

--- arability/app/controllers/search_controller.rb
+++ arability/app/controllers/search_controller.rb
@@ -21,7 +21,8 @@ class SearchController < BackendController
   #     similar keywords were found
   def search_keywords
     @categories = params[:categories]
-    @project_id = params[:project_id]
+    @developer_id = Developer.find_by_gamer_id(current_gamer.id).id
+    @projects = Project.where(owner_id: @developer_id).all
     if @categories.present?
       categories_array = @categories.split(/,/)
       categories_array.map! { |x| x.strip }
@@ -56,7 +57,8 @@ class SearchController < BackendController
 	#			returns an empty list if the search keyword has no synonyms
   def search
     @search_keyword = params["search"]
-    @project_id = params[:project_id]
+    @developer_id = Developer.find_by_gamer_id(current_gamer.id).id
+    @projects = Project.where(owner_id: @developer_id).all
     @country = params["country"]
     @age_from = params["age_from"]
     @age_from = @age_from.to_i if !@age_from.blank?
