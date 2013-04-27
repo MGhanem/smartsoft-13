bd1a019d4d643e26665e6f45e2e758dd6e0e9c75
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-27 17:56:02 +0200

    [WIP][issue #369] tests implementation in progress

--- arability/spec/controllers/admin_controller_spec.rb
+++ arability/spec/controllers/admin_controller_spec.rb
@@ -3,6 +3,16 @@ require "spec_helper"
 
 describe AdminController  do
 	
+	let(:model1){
+	  model = SubscriptionModel.new
+    model.name = "Test"
+    model.limit_search = "300"
+    model.limit_follow = "200"
+    model.limit_project = "100"
+    model.save
+    model
+  }
+
 	it "should login the user if correct username and password" do
 		get :login
 		expect(response.code).to eq("200")
@@ -17,5 +27,16 @@ describe AdminController  do
       get :login
       expect(response).to render_template("login")
     end
+
+    it "list all subscription models" do
+      get :subscription_model
+      puts response.inspect
+      response.code.should eq("302")
+    end
+
+    # it "list all subscription modelss" do
+    #   get :edit_subscription_model, model_id: model1.id
+    #   response.code.should eq("302")
+    # end
     
 end
