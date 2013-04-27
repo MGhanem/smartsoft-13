1ac412e941fd3fc9ba7482bf49b0b0a83a37ac64
Omar Hossam <omar.mohy-eldin@student.guc.edu.eg>
2013-04-28 00:53:38 +0200

    [WIP][issue #369] tests added

--- arability/spec/controllers/admin_controller_spec.rb
+++ arability/spec/controllers/admin_controller_spec.rb
@@ -5,48 +5,37 @@ include RequestHelpers
 include Warden::Test::Helpers
 
 describe AdminController  do
-  describe "GET #subscription_model" do
-    include Devise::TestHelpers
+  describe "GET #edit_subscription_model" do
     let(:model1) do
-    model = SubscriptionModel.new
-    model.name = "Test"
-    model.limit_search = "300"
-    model.limit_follow = "200"
-    model.limit_project = "100"
-    model.save
-    model
-  end
+      model = SubscriptionModel.new
+      model.name = "Test"
+      model.limit_search = "300"
+      model.limit_follow = "200"
+      model.limit_project = "100"
+      model.save validate: false
+      model
+    end
     it "list all subscription models" do
       model1
       post :login, username: "admin", password: "admin"
       get :subscription_model
-      assigns(:@models).should =~ [model1]
+      assigns(:models).should =~ [model1]
     end
+    it "should view subscription model needed to be tested" do
+      model1
+      post :login, username: "admin", password: "admin"
+      get :edit_subscription_model, errors: nil, model_id: model1.id
+      assigns(:model).should eq model1  
+    end
+    it "should edit subscription model" do
+      model1
+      put :update_subscription_model, model_id: model1.id, subscription_model: {name: "Try", limit_search: "100", limit_follow: "200", limit_project: "300"}
+      assigns(:model).should_not eq model1
+    end
+    it "should not edit subscription model due to wrong data" do
+      model1
+      put :update_subscription_model, model_id: model1.id, subscription_model: {name: "", limit_search: "100", limit_follow: "200", limit_project: "300"}
+      assigns(:model).should eq nil
+    end    
   end
-end	
-	
-
-    
-
-	# it "should login the user if correct username and password" do
-	# 	get :login
-	# 	expect(response.code).to eq("200")
-	# end
-
-	# it "should redirect to login page" do
-	# 	get :index
-	# 	response.should redirect_to :action => :login
-	# end
-
-	# it "renders the login template" do
- #      get :login
- #      expect(response).to render_template("login")
- #    end
-
-    
-
- #    # it "list all subscription modelss" do
- #    #   get :edit_subscription_model, model_id: model1.id
- #    #   response.code.should eq("302")
- #    # end
- #  end  
+end
