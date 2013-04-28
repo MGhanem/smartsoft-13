d008cb8b26eaf0dddda0d4617fe2150ec8f8f9a9
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-28 17:01:31 +0200

    [Issue #195][WIP] added action for synonym retrival via ajax as json object

--- arability/app/controllers/search_controller.rb
+++ arability/app/controllers/search_controller.rb
@@ -63,32 +63,77 @@ class SearchController < BackendController
 	#			returns an empty list if the search keyword has no synonyms
   def search
     @search_keyword = params["search"]
-    @project_id = params[:project_id]
+
+    if !@search_keyword.blank?
+      @search_keyword = @search_keyword.strip
+      @search_keyword = @search_keyword.split(" ").join(" ")
+    
+      @no_synonyms_found = false
+      @search_keyword_model = Keyword.find_by_name(@search_keyword)
+      if !@search_keyword_model.blank?
+        @synonyms, @votes =
+          @search_keyword_model.retrieve_synonyms
+    
+        @no_synonyms_found = true if @synonyms.blank?
+    
+        @total_votes = 0
+        @votes.each { |synonym_id, synonym_votes| @total_votes += synonym_votes }
+      else
+        redirect_to search_keywords_path(search: @search_keyword)
+      end
+    else
+      redirect_to search_keywords_path
+    end
+  end
+
+    #Description:
+  #   search for synonyms for a particular keyword
+  # Author:
+  #   Nourhan Mohamed
+  # params:
+  #   search: a string representing the search keyword, from the params list
+  #     from a textbox in the search_keywords view
+  #   country: a string representing country filter, maybe nil
+  #   age_from: a string representing age lower bound filter, maybe nil
+  #   age_to: a string representing age upper bound filter, maybe nil
+  #   education: a string representing education level filter, maybe nil
+  #   gender: a string representing gender filter, maybe nil
+  # returns:
+  #   success: 
+  #     returns to the search view a list of synonyms for the keyword
+  #     sorted by vote count (according to optional filters)
+  #   failure:
+  #     returns an empty list if the search keyword has no synonyms
+  def search_with_filters
+    search_keyword = params["search"]
     @country = params["country"]
     @age_from = params["age_from"]
     @age_from = @age_from.to_i if !@age_from.blank?
     @age_to = params["age_to"]
     @age_to = @age_to.to_i if !@age_to.blank?
-    if !@age_from.blank? && !@age_to.blank? && @age_from > @age_to
+    @gender = params["gender"]
+    @education = params["education"]
+
+    if !@age_from.blank? && !@age_to.blank? && @age_from.to_i > @age_to.to_i
       temp = @age_from
       @age_from = @age_to
       @age_to = temp
     end
-    @gender = params["gender"]
-    @education = params["education"]
-    if !@search_keyword.blank?
-      @search_keyword = @search_keyword.strip
-      @search_keyword = @search_keyword.split(" ").join(" ")
-      @no_synonyms_found = false
-      @search_keyword_model = Keyword.find_by_name(@search_keyword)
+
+    if !search_keyword.blank?
+      search_keyword = search_keyword.strip
+      search_keyword = search_keyword.split(" ").join(" ")
+
+      @search_keyword_model = Keyword.find_by_name(search_keyword)
       if !@search_keyword_model.blank?
         @synonyms, @votes =
           @search_keyword_model.retrieve_synonyms(@country, @age_from, @age_to, @gender, @education)
-        @no_synonyms_found = true if @synonyms.blank?
+
         @total_votes = 0
         @votes.each { |synonym_id, synonym_votes| @total_votes += synonym_votes }
+        render json: { synonyms: @synonyms, votes: @votes }
       else
-        redirect_to search_keywords_path(search: @search_keyword)
+        redirect_to search_keywords_path(search: search_keyword)
       end
     else
       redirect_to search_keywords_path
