0e34c9678bbc92f56831ff98c085fadccc24e4aa
Mirna Yacout <mirna.yacout@gmail.com>
2013-04-28 11:52:08 +0200

    [#198][#199][WIP] toggle buttons working fine

--- arability/config/routes.rb
+++ arability/config/routes.rb
@@ -39,7 +39,11 @@ Arability::Application.routes.draw do
     get "games/showprizes"
     get "games/get_score_only"
     post "games/record_synonym"
+    get "/games/fame"
     get "/games/halloffame"
+    get "/games/facebookhalloffame"
+    get "/games/twitterhalloffame"
+    get "/games/mainhalloffame"
 
     match "/share_on_facebook"=>'games#post_score_facebook', :as => "share_on_facebook"
     get "/games/disconnect_facebook"
