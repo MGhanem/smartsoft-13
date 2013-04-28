ffdcdca32b3be3824592cda8be89c251fe0c282f
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-04-28 21:33:26 +0200

    [Issue #195][WIP] updated test cases for search controllers

--- arability/spec/controllers/search_controller_spec.rb
+++ arability/spec/controllers/search_controller_spec.rb
@@ -27,6 +27,38 @@ describe SearchController do
       keyword
     end
 
+    let(:s) {
+      k
+      s = Synonym.create(name: "ابت", keyword_id: k.id, approved: true)
+      s
+    }
+
+    let(:s1) {
+      k
+      s = Synonym.create(name: "ابتث", keyword_id: k.id, approved: true)
+      s
+    }
+
+    let(:s2) {
+      k
+      s = Synonym.create(name: "ابتثج", keyword_id: k.id, approved: false)
+      s
+    }
+
+    let(:gamer_vote_s) {
+      gamer
+      s
+      success, vote = Vote.record_vote(gamer.id, s.id)
+      vote
+    }
+
+    let(:gamer2_vote_s1) {
+      gamer2
+      s1
+      success, vote = Vote.record_vote(gamer2.id, s1.id)
+      vote
+    }
+
     let(:gamer) do
       gamer = Gamer.new
       gamer.username = "Nourhan"
@@ -35,7 +67,22 @@ describe SearchController do
       gamer.date_of_birth = "1993-03-23"
       gamer.email = "nour@gmail.com"
       gamer.password = "1234567"
+      gamer.education_level = "medium"
       gamer.save validate: false
+      gamer
+    end
+
+    let(:gamer2) do
+      gamer = Gamer.new
+      gamer.username = "Nourhan"
+      gamer.country = "Qattar"
+      gamer.gender = "female"
+      gamer.date_of_birth = "1993-03-23"
+      gamer.email = "nour@gmail.com"
+      gamer.password = "1234567"
+      gamer.education_level = "high"
+      gamer.save validate: false
+      gamer
     end
 
     it "should get only keywords in category" do
@@ -61,7 +108,71 @@ describe SearchController do
       k
       k2
       get :search_keywords, search: "test"
-      assigns(:similar_keywords).should =~ [k, k2]
+      assigns(:similar_keywords).should eq([k, k2])
+    end
+
+    it "should list synonyms according to vote count" do
+      d = create_logged_in_developer
+      sign_in(d.gamer)
+      k
+      s
+      s1
+      s2
+      gamer_vote_s
+      get :search, search: "test    "
+      assigns(:synonyms).should eq([s, s1])
+      assigns(:votes)[1].should eq(1)
+      assigns(:votes)[2].should be_nil
+    end
+
+    it "should list synonyms according to vote count upon filtering" do
+      d = create_logged_in_developer
+      sign_in(d.gamer)
+      k
+      s
+      s1
+      s2
+      gamer_vote_s
+
+      get :search_with_filters, search: "test", country: "Qattar", age_from: "40",
+        age_to: "19", education: "high", gender: "female"
+      assigns(:synonyms).should eq([s, s1])
+      assigns(:votes)[1].should be_nil
+      assigns(:votes)[2].should be_nil
+
+      get :search_with_filters, search: "test", country: "Egypt", age_from: "40",
+        age_to: "19", gender: "female", education: "medium"
+      assigns(:synonyms).should eq([s, s1])
+      assigns(:votes)[1].should eq(1)
+      assigns(:votes)[2].should be_nil
+    end
+
+    it "should redirect to search_keywords action if passed a non existing keyword 
+      to search or search_by_filters action" do
+      d = create_logged_in_developer
+      sign_in(d.gamer)
+
+      get :search, search: "abc"
+      response.code.should eq("302")
+      response.should redirect_to(search_keywords_path(search: "abc"))
+
+      get :search_with_filters, search: "abc"
+      response.code.should eq("302")
+      response.should redirect_to(search_keywords_path(search: "abc"))
+    end
+
+    it "should redirect to search_keywords action if passed an empty string 
+      to search or search_by_filters action" do
+      d = create_logged_in_developer
+      sign_in(d.gamer)
+
+      get :search, search: ""
+      response.code.should eq("302")
+      response.should redirect_to(search_keywords_path)
+
+      get :search_with_filters, search: ""
+      response.code.should eq("302")
+      response.should redirect_to(search_keywords_path)
     end
   end
 end
