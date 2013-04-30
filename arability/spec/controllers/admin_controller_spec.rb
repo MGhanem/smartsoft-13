c1eac41e4c141f8447f1cfa615dfe3e504c3885a
Karim El Naggar <karimfarid.naggar@gmail.com>
2013-04-30 22:04:14 +0200

    fixed merge conflict

--- arability/spec/controllers/admin_controller_spec.rb
+++ arability/spec/controllers/admin_controller_spec.rb
@@ -41,21 +41,6 @@ describe AdminController  do
       gamer
     end
 
-	  # it "should login the user if correct username and password" do
-		 #  get :login
-		 #  expect(response.code).to eq("200")
-	  # end
-
-	  # it "should redirect to login page" do
-		 #  get :index
-		 #  response.should redirect_to :action => :login
-	  # end
-
-	  # it "renders the login template" do
-   #    get :login
-   #    expect(response).to render_template("login")
-   #  end
-
     it "list all subscription models" do
       model1
       gamer
