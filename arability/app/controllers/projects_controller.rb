9fad679dadb4ca698f3dd221308caec8d7198be4
Noha Hesham <nohahesham16@gmail.com>
2013-05-01 23:22:02 +0200

    MC

--- arability/app/controllers/projects_controller.rb
+++ arability/app/controllers/projects_controller.rb
@@ -212,27 +212,6 @@ def show
   end
 end
 
- # Author:
- #    Noha Hesham
- # Description:
- #    Finds the project by its id then destroys it
- # Params:
- #    Project id
- # Success:
- #    A pop up appears and makes sure the user wants to
- #    delete the project by choosing ok the
- #    project is successfully deleted
- # Failure:
- #    Project is not deleted
-  def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
-  end
-
   # Author:
   #   Mohamed Tamer
   # Description:
