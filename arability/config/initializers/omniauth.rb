5dd3d41c0b1af657244e2f2b5042121a67de40a4
Amr Hatem Abdelraouf <amr.abdelraouf92@gmail.com>
2013-05-01 18:20:59 +0200

    [#361] stashes

--- arability/config/initializers/omniauth.rb
+++ arability/config/initializers/omniauth.rb
@@ -1,13 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # configuring twitter
   provider :twitter, "y3D1eGgnppUGjKNP6C47GQ", "37WC5Xa3HnawQe5A6YIMYSsTmUSzkKuky6gOpzD2I"
-  # configuring facebook
   provider :facebook, "155521981280754", "a4d4684f0554e3637085e72eaa132890",
-  {scope: "email, offline_access, publish_stream",
+    {scope: "email, offline_access, publish_stream",
   	client_options: {ssl: {ca_path: "/etc/ssl/certs"}}}
-  configuring google
   provider :google_oauth2, ENV["572595320157.apps.googleusercontent.com"],
-   ENV["Z3mU_zT6UtUhOqhNc-iQHqpk"]
+    ENV["Z3mU_zT6UtUhOqhNc-iQHqpk"]
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure  
   }
