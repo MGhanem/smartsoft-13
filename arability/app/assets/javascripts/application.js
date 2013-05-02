2605a1652ba9f68f984ccaae6576705a12700b38
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-05-02 21:20:16 +0200

    [Issue #186] [DONE]

--- arability/app/assets/javascripts/application.js
+++ arability/app/assets/javascripts/application.js
@@ -36,9 +36,9 @@ function checkboxlimit(checkgroup, limit, locale){
 	for (var i=0; i<checkgroup.length; i++){
 		checkgroup[i].onclick=function(){
 		var checkedcount = 0;
-		for (var i=0; i<checkgroup.length; i++)
-			checkedcount+=(checkgroup[i].checked)? 1 : 0
-		  if (checkedcount>limit){
+		for (var i = 0; i < checkgroup.length; i++)
+			checkedcount += (checkgroup[i].checked)? 1 : 0
+		  if (checkedcount > limit){
 		  	if (locale == "en") {
 		  	  alert("You can only select a maximum of " + limit + " checkboxes");
 		  	} else {
