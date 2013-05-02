e1e562ac724887f2358e60088f9de7f6b5770c35
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-05-02 21:08:58 +0200

    [Issue #186] [DONE]

--- arability/app/assets/javascripts/application.js
+++ arability/app/assets/javascripts/application.js
@@ -31,11 +31,11 @@
 // failure:
 //   none
 function checkboxlimit(checkgroup, limit, locale){
-	var checkgroup=checkgroup;
-	var limit=limit;
+	var checkgroup = checkgroup;
+	var limit = limit;
 	for (var i=0; i<checkgroup.length; i++){
 		checkgroup[i].onclick=function(){
-		var checkedcount=0;
+		var checkedcount = 0;
 		for (var i=0; i<checkgroup.length; i++)
 			checkedcount+=(checkgroup[i].checked)? 1 : 0
 		  if (checkedcount>limit){
