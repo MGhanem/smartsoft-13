b4b6e492de03e9e9481357b18e4023fac3d13980
Amr Hatem Abdelraouf <amr.abdelraouf92@gmail.com>
2013-05-02 17:03:00 +0200

    [#361] adds tests (incomplete)

--- arability/spec/controllers/google_authentication_controller_spec.rb
+++ arability/spec/controllers/google_authentication_controller_spec.rb
@@ -6,14 +6,14 @@ include RequestHelpers
 describe AuthenticationsController do
 
   before do
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook] 
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2] 
   end
 
   it "should redirect to the new_social page" do
     I18n.locale = :ar
-    get :facebook_callback
+    get :google_callback
     response.should redirect_to "/ar/social_registrations/new_social?" +
-    "email=email%40mock.com&gender=male&provider=facebook"
+    "email=email%40mock.com&gender=male&provider=google_oauth2"
   end
 
   it "should redirect to home page, sign the user in and create an auth in his name" do
@@ -28,7 +28,7 @@ describe AuthenticationsController do
     g.confirmed_at = Time.now
     g.save validate: false
     I18n.locale = :ar
-    get :facebook_callback
+    get :google_callback
     response.should redirect_to root_url
     expect(Authentication.exists?(gamer_id: g.id)).to eq(true)
   end
@@ -45,32 +45,15 @@ describe AuthenticationsController do
     g.confirmed_at = Time.now
     g.save validate: false
     Authentication.create_with_omniauth(
-      "facebook",
+      "google",
        "011252217", 
        "mock123token456", 
        nil, 
        "email@mock.com", 
        g.id)
     I18n.locale = :ar
-    get :facebook_callback
+    get :google_callback
     response.should redirect_to root_url
-  end
-
-  it "should redirect to edit gamer since he is requesting a link" do
-    g = Gamer.new
-    g.username = "test"
-    g.country = "Egypt"
-    g.education_level = "school"
-    g.gender = "male"
-    g.date_of_birth = "1993-11-23"
-    g.email = "email@mock.com"
-    g.password = "123456"
-    g.confirmed_at = Time.now
-    g.save validate: false
-    login_gamer(g)
-    I18n.locale = :ar
-    get :facebook_callback
-    response.should redirect_to "/gamers/edit"
   end
 
 end
