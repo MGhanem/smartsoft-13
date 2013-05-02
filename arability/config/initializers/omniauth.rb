3e5d578fb964719876d09a18f3cd20e8e3eae913
Amr Hatem Abdelraouf <amr.abdelraouf92@gmail.com>
2013-05-02 02:40:48 +0200

    [#361] modifies OR in navbar

--- arability/config/initializers/omniauth.rb
+++ arability/config/initializers/omniauth.rb
@@ -1,10 +1,18 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  require 'openid/store/filesystem'
+
   provider :twitter, "y3D1eGgnppUGjKNP6C47GQ", "37WC5Xa3HnawQe5A6YIMYSsTmUSzkKuky6gOpzD2I"
+
   provider :facebook, "155521981280754", "a4d4684f0554e3637085e72eaa132890",
     {scope: "email, offline_access, publish_stream",
   	client_options: {ssl: {ca_path: "/etc/ssl/certs"}}}
-  provider :google_oauth2, ENV["572595320157.apps.googleusercontent.com"],
-    ENV["Z3mU_zT6UtUhOqhNc-iQHqpk"]
+
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google',
+    :identifier => 'https://www.google.com/accounts/o8/id'
+
+  provider :google_oauth2, "758114528235-3b33edh32qsu3mfnqpqd3n1v9e3ov4ql.apps.googleusercontent.com",
+    "ittGQy5A3HcGMG2lXnNQ3t_X"
+
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure  
   }
