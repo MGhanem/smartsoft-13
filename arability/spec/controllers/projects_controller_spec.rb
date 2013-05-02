5ec4173f78d4b6d9abc7e07acec05197e3f5b9f9
Salma <salma@Brownie.(none)>
2013-05-02 22:28:01 +0200

    [MC] Switching to another branch

--- arability/spec/controllers/projects_controller_spec.rb
+++ arability/spec/controllers/projects_controller_spec.rb
@@ -1,19 +1,10 @@
 #encoding: UTF-8
 require "spec_helper"
 require "request_helpers"
-<<<<<<< HEAD
 include RequestHelpers
 include Warden::Test::Helpers
 
 describe ProjectsController, type: :controller do
-
-=======
-include Warden::Test::Helpers
-include RequestHelpers
-
-describe ProjectsController do
-  include Devise::TestHelpers
->>>>>>> 2592032d3f4a8f833f86c106e736b05d2774b034
 
   let(:gamer1){
 	  gamer = Gamer.new
@@ -59,14 +50,6 @@ describe ProjectsController do
     syn
   }
 
-<<<<<<< HEAD
-  it "a developer can open the link of import of one of his projects" do
-    sign_in developer1.gamer
-    get :import_csv, :project_id => project.id
-    page.should have_content(I18n.t(:import_csv_title))
-  end
-
-=======
   #Timo's tests
   describe "GET #index" do
     it "populates an array of projects" do
@@ -151,102 +134,6 @@ describe ProjectsController do
   end
   #End of Timo's tests
 
-  #Salma's Tests
-  describe "GET #new" do
-    it "initializes a new project" do
-      a = create_logged_in_developer
-      sign_in(a.gamer)
-      get :new
-    end
-  end
-
-describe "GET #create" do
-  context "with valid attributes" do
-    it "assigns attributes to the new project" do
-      expect{
-        project :create
-      }
-   end
-
-    it "redirects to the project index" do
-      project
-      post :create, project: project
-      response.should redirect_to Project.index
-    end
-  end
-
-  context "with invalid attributes" do
-    it "does not save the new project" do
-      expect{
-        project
-        post :create, project: project
-      }.to_not change(Project,:count)
-    end
-
-    it "re-renders the new method" do
-      post :create, project: Factory.attributes_for(:invalid_project)
-      response.should render_template :new
-    end
-  end
-end
-
-describe "GET #edit" do
-  it "assigns the requested project to @project" do
-    project
-    get :edit, id: project
-    assigns(:project).should eq(project)
-  end
-end
-
-describe 'PUT update' do
-  before :each do
-    @project = Factory(:project, name: "Pro", minAge:"23", maxAge:"50")
-  end
-
-  context "valid attributes" do
-    it "located the requested @project" do
-      project
-      put :update, id: @project, project: project
-      assigns(:project).should eq(@project)
-    end
-
-    it "changes @project's attributes" do
-      put :update, id: @project,
-        project: Factory.attributes_for(:project, name: "Pro", minAge:"23", maxAge:"50")
-      @project.reload
-      @project.name.should eq("Pro")
-      @project.minAge.should eq("23")
-      @project.maxAge.should eq("50")
-    end
-
-    it "redirects to the project index" do
-      project
-      put :update, id: @project, project: project
-      response.should redirect_to projects_path
-    end
-  end
-
-  context "invalid attributes" do
-    it "locates the requested @project" do
-      put :update, id: @project, project: Factory.attributes_for(:invalid_project)
-      assigns(:project).should eq(@project)
-    end
-
-    it "does not change @project's attributes" do
-      put :update, id: @project,
-        project: Factory.attributes_for(:project, name: "Pro", minAge: "23", maxAge:nil)
-      @project.reload
-      @project.name.should_not eq("Pro")
-      @project.minAge.should_not eq("23")
-      @project.minAge.should eq("50")
-    end
-
-    it "re-renders the edit method" do
-      put :update, id: @project, project: Factory.attributes_for(:invalid_project)
-      response.should render_template :edit
-    end
-  end
-
 
   # Noha's test
   it "should make developer remove a project shared with him" do
@@ -261,7 +148,6 @@ describe 'PUT update' do
     response.code.should eq("302")
   end
 
->>>>>>> 2592032d3f4a8f833f86c106e736b05d2774b034
   #khloud's tests
 
   it "redirects to project path after calling export_to_csv if project empty" do
@@ -305,9 +191,5 @@ describe 'PUT update' do
     get :export_to_json, project_id: p.id
     response.code.should eq("200")
   end
-<<<<<<< HEAD
 
-=======
-end
->>>>>>> 2592032d3f4a8f833f86c106e736b05d2774b034
 end
